782af614afc0cb721fa82d38e4c1c5a3bdbabff0
polo <826770122@qq.com>
2022-11-17 15:45:07 +0800

    fix: modify productionSourceMap

--- webfe/package_vue/config/index.js
+++ webfe/package_vue/config/index.js
@@ -27,7 +27,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static/',
     assetsPublicPath: '/',
-    productionSourceMap: process.env.NODE_ENV !== 'production',
+    productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
     // Before setting to `true`, make sure to:

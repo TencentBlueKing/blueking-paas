9672bfc01be55041c2fd49f37566b8360731a907
leafage-collb <3303710737@qq.com>
2023-01-09 11:15:07 +0800

    fix: 部署页面自适应

--- webfe/package_vue/src/views/plugin-center/plugin/version-manager/version-release.vue
+++ webfe/package_vue/src/views/plugin-center/plugin/version-manager/version-release.vue
@@ -1022,9 +1022,6 @@
         color: #3A84FF;
     }
 }
-.container.visible-range-release {
-     width: 85vw !important;
-}
 .visible-range-release .app-container {
     margin-top: 0;
     padding-top: 0;

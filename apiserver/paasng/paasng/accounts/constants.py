da625a340e7cb83cf1629600776f1d41eef6856a
shabbywu <shabbywu@tencent.com>
2023-02-09 18:23:50 +0800

    minor: allow enable plugin center in settings (#190)

--- apiserver/paasng/paasng/accounts/constants.py
+++ apiserver/paasng/paasng/accounts/constants.py
@@ -89,7 +89,7 @@ class AccountFeatureFlag(ChoicesEnum):
             ALLOW_CREATE_SMART_APP: settings.IS_ALLOW_CREATE_SMART_APP_BY_DEFAULT,
             ALLOW_CREATE_CLOUD_NATIVE_APP: settings.IS_ALLOW_CREATE_CLOUD_NATIVE_APP_BY_DEFAULT,
             ENABLE_TC_DOCKER: False,
-            ALLOW_PLUGIN_CENTER: False,
+            ALLOW_PLUGIN_CENTER: settings.IS_ALLOW_PLUGIN_CENTER,
         }
     )
 

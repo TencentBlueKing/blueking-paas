6e5690da8bc6cfebe559e9a8c9f772df0acdcdb7
polo <826770122@qq.com>
2023-04-18 21:17:13 +0800

    feat: 完成自定义插件开发

--- webfe/package_vue/pre-task-plugin.js
+++ webfe/package_vue/pre-task-plugin.js
@@ -23,7 +23,7 @@ const APP_VERSION = process.env.APP_VERSION || 'ee';
 
 const JSON_DIR_PATH = path.resolve(__dirname, './static/json');
 const JS_DIR_PATH = path.resolve(__dirname, './static/js');
-const RUNTIME_DIR_PATH = path.resolve(__dirname, './static/runtime');
+// const RUNTIME_DIR_PATH = path.resolve(__dirname, './static/runtime');
 
 class PreTaskPlugin {
   constructor() {
@@ -31,7 +31,7 @@ class PreTaskPlugin {
     console.time('Pre task');
     const paasStaticPath = path.resolve(JSON_DIR_PATH, './paas_static.js');
     const bklogoutPath = path.resolve(JS_DIR_PATH, './bklogout.js');
-    const runtimePath = path.resolve(RUNTIME_DIR_PATH, './runtime.js');
+    // const runtimePath = path.resolve(RUNTIME_DIR_PATH, './runtime.js');
 
     if (fs.existsSync(paasStaticPath)) {
       fs.unlinkSync(paasStaticPath);
@@ -47,27 +47,27 @@ class PreTaskPlugin {
     fs.writeFileSync(paasStaticPath, staticData);
 
     // 复制runtime
-    const PAASRUNTIME = APP_VERSION === 'te' ? `../../webfe-settings/runtime.${APP_VERSION}.js` : `../../static/runtime/runtime.${APP_VERSION}.js`;
-    const runtimeData = fs.readFileSync(path.resolve(RUNTIME_DIR_PATH, PAASRUNTIME));
-    let envs = {};
-    let fileContent = runtimeData.toString();
+    // const PAASRUNTIME = APP_VERSION === 'te' ? `../../webfe-settings/runtime.${APP_VERSION}.js` : `../../static/runtime/runtime.${APP_VERSION}.js`;
+    // const runtimeData = fs.readFileSync(path.resolve(RUNTIME_DIR_PATH, PAASRUNTIME));
+    // let envs = {};
+    // // let fileContent = runtimeData.toString();
 
-    if (process.env.NODE_ENV === 'production') {
-      envs = Object.assign({}, {
-        NODE_ENV: 'production',
-      }, process.env);
-    } else {
-      envs = Object.assign({}, {
-        NODE_ENV: 'development',
-      }, process.env);
-    }
-    for (const key in envs) {
-      const reg = new RegExp(`<%=${key}%>`);
-      fileContent = fileContent.replace(reg, JSON.stringify(envs[key]));
-    }
+    // if (process.env.NODE_ENV === 'production') {
+    //   envs = Object.assign({}, {
+    //     NODE_ENV: 'production',
+    //   }, process.env);
+    // } else {
+    //   envs = Object.assign({}, {
+    //     NODE_ENV: 'development',
+    //   }, process.env);
+    // }
+    // for (const key in envs) {
+    //   const reg = new RegExp(`<%=${key}%>`);
+    //   fileContent = fileContent.replace(reg, JSON.stringify(envs[key]));
+    // }
 
-    fileContent = fileContent.replace(/<%=[\w]+%>/g, '\'\'');
-    fs.writeFileSync(runtimePath, fileContent);
+    // fileContent = fileContent.replace(/<%=[\w]+%>/g, '\'\'');
+    // fs.writeFileSync(runtimePath, fileContent);
 
     // 复制bklogout
     const PAASLOGOUT = APP_VERSION === 'te' ? `../../webfe-settings/bklogout.${APP_VERSION}.js` : `../../static/js/bklogout.${APP_VERSION}.js`;

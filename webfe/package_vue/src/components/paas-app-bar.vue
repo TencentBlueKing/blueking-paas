18882e7dfa519a8fc491ccfede15281ffd0fdf41
wmh <3303710797@qq.com>
2023-05-22 19:22:20 +0800

    Wmh bug dev (#447)
    Co-authored-by: leafage-collb <3303710737@qq.com>

--- webfe/package_vue/src/components/paas-app-bar.vue
+++ webfe/package_vue/src/components/paas-app-bar.vue
@@ -51,7 +51,10 @@
             position: 'bottom left'
           }"
         >
-          <div slot="trigger">
+          <div
+            slot="trigger"
+            class="module-wrapper"
+          >
             <a
               href="javascript: void(0);"
               class="module-name"
@@ -116,7 +119,10 @@
             position: 'bottom left'
           }"
         >
-          <div slot="trigger">
+        <div
+          slot="trigger"
+          class="module-wrapper"
+        >
             <a
               href="javascript: void(0);"
               class="module-name time-text"
@@ -240,6 +246,12 @@
                 return this.curAppModule.source_origin === this.GLOBAL.APP_TYPES.SMART_APP;
             }
         },
+        mounted () {
+          window.addEventListener('click', this.closeDropdown);
+          this.$once('hook:beforeDestroy', ()=>{
+            window.removeEventListener('click', this.closeDropdown);
+          });
+        },
         methods: {
             handleModuleSelect (module) {
                 this.$refs.dropdown.close();
@@ -283,6 +295,12 @@
                 const startTime = formatDate(date[0], 'YYYY-MM-DD');
                 const endTime = formatDate(date[1], 'YYYY-MM-DD');
                 this.$emit('change-date', [startTime, endTime], id);
+            },
+            closeDropdown (e) {
+              const targetEl = document.querySelector('.module-wrapper')
+              if (!targetEl?.contains(e.target)) {
+                this.$refs.dropdown?.hide(e.target);
+              }
             }
         }
     };
@@ -359,6 +377,10 @@
             &.time-text {
                 font-size: 12px
             }
+        }
+
+        .module-wrapper .module-name {
+            line-height: 42px;
         }
 
         .icon-time-cls {

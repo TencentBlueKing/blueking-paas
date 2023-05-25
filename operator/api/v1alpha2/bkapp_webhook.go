1575c8e67a1fdde3c2d316f3b5b64f1cd6855997
jamesgetx <gejun.coolfriend@gmail.com>
2023-05-25 17:11:21 +0800

    style: minor
    N

--- operator/api/v1alpha2/bkapp_webhook.go
+++ operator/api/v1alpha2/bkapp_webhook.go
@@ -29,7 +29,6 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/validation/field"
-
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"

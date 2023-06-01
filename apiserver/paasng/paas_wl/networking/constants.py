7bb33fc49dc367a0dcdf96a2c4e03661cdc1c4c1
schnee <suzh9@mail2.sysu.edu.cn>
2023-06-01 14:23:18 +0800

    feat: admin42 access bcs egress operator (#474)

--- apiserver/paasng/paas_wl/networking/constants.py
+++ apiserver/paasng/paas_wl/networking/constants.py
@@ -16,19 +16,9 @@ We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
 
-from paas_wl.networking.ingress.exceptions import DefaultServiceNameRequired, EmptyAppIngressError
-from paas_wl.networking.ingress.managers import AppDefaultIngresses
-from paas_wl.workloads.processes.controllers import env_is_running
-from paasng.platform.applications.models import ModuleEnvironment
+from blue_krill.data_types.enum import EnumField, StructuredEnum
 
 
-def sync_proc_ingresses(env: ModuleEnvironment):
-    """Sync ingresses configs of the given env"""
-    if not env_is_running(env):
-        return
-
-    for mgr in AppDefaultIngresses(env.wl_app).list():
-        try:
-            mgr.sync()
-        except (DefaultServiceNameRequired, EmptyAppIngressError):
-            continue
+class NetworkProtocol(str, StructuredEnum):
+    TCP = EnumField('TCP', label='TCP')
+    UDP = EnumField('UDP', label='UDP')

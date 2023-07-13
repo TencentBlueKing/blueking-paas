10aa5e80bdbe660e94eef23ca238acf18cdca2af
shabbywu <shabbywu@tencent.com>
2023-07-13 17:07:30 +0800

    fix unittest

--- apiserver/paasng/tests/paas_wl/workloads/processes/test_managers.py
+++ apiserver/paasng/tests/paas_wl/workloads/processes/test_managers.py
@@ -122,7 +122,7 @@ class TestProcInstManager:
     def test_get_logs(self, wl_app, pod):
         # Query process instances
         inst = instance_kmodel.list_by_process_type(wl_app, 'web')[0]
-        with mock.patch('paas_wl.workloads.processes.models.Instance.Meta.kres_class') as kp:
+        with mock.patch('paas_wl.workloads.processes.entities.Instance.Meta.kres_class') as kp:
             instance_kmodel.get_logs(inst)
             assert kp().get_log.called
 

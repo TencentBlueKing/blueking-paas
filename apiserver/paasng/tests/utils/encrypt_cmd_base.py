bfaf7125950e1136bc43476b4b9503b5a3060d95
SheepSheepChen <527738616@qq.com>
2023-08-14 16:40:55 +0800

    feat:support sm4 encryption algorithm;feat:add encryption migration command;

--- apiserver/paasng/tests/utils/encrypt_cmd_base.py
+++ apiserver/paasng/tests/utils/encrypt_cmd_base.py
@@ -19,22 +19,20 @@ to the current version of the project delivered to anyone in the future.
 from typing import Any, Dict, Tuple
 
 import pytest
-from blue_krill.encrypt.handler import EncryptHandler
 from blue_krill.models.fields import EncryptField
 from django.core.management import call_command
+from django.test import override_settings
 from django_dynamic_fixture import G
 
 from tests.utils.helpers import generate_random_string
 
 
-def generate_fernet_encrypted_instance(model_class):
+def generate_encrypted_instance(model_class):
     """创建一个加密模型，并生成加密字段"""
 
     unencryped_field_dict: Dict[str, str] = {}
     instance = G(model_class)
     update_fields = []
-
-    handler = EncryptHandler(encrypt_cipher_type='FernetCipher')
 
     for field in model_class._meta.fields:
         if isinstance(field, EncryptField):
@@ -42,8 +40,7 @@ def generate_fernet_encrypted_instance(model_class):
 
             random_str = generate_random_string()
             unencryped_field_dict[field.name] = random_str
-            encryped = handler.encrypt(random_str)
-            setattr(instance, field.name, encryped)
+            setattr(instance, field.name, random_str)
 
     instance.save(update_fields=update_fields)
     return instance, unencryped_field_dict
@@ -55,6 +52,7 @@ def assert_fields_encryped(pk, model_class, unencryped_field_dict, encrypt_heade
     :param pk: 实例主键
     :param model_class: 实例类
     :param unencryped_field_dict: 未加密字段字典
+    :param encrypt_header: 加密算法对应的加密头
     """
 
     select_dict: Dict = {}
@@ -70,30 +68,42 @@ def assert_fields_encryped(pk, model_class, unencryped_field_dict, encrypt_heade
             assert field_value == unencryped_field_dict[field.name]
 
 
-def assert_fields_encryped_with_sm4(pk, model_class, unencryped_field_dict):
-    assert_fields_encryped(
-        pk=pk, model_class=model_class, unencryped_field_dict=unencryped_field_dict, encrypt_header="sm4ctr$"
-    )
-
-
-def assert_fields_encryped_fernet(pk, model_class, unencryped_field_dict):
-    assert_fields_encryped(
-        pk=pk, model_class=model_class, unencryped_field_dict=unencryped_field_dict, encrypt_header="bkcrypt$"
-    )
+def get_encrypt_header(encrypt_type):
+    """
+    获取加密算法对应的加密头
+    :param encrypt_type: 加密算法类型
+    """
+    encrypt_header_dict = {'FernetCipher': 'bkcrypt$', 'SM4CTR': 'sm4ctr$'}
+    try:
+        return encrypt_header_dict[encrypt_type]
+    except KeyError:
+        raise ValueError(f"Invalid encrypt type: {encrypt_type}")
 
 
 class BaseTestEnctrypMigrationCmd:
     @pytest.fixture(autouse=True)
-    def generate_fernet_encrypted_instances(self, app_models):
-        unencrypted_dict: Dict[Any, Tuple[Any, Dict[str, str]]] = {}
+    def generate_legacy_encrypted_instances(self, app_models, legacy_encrypt_type):
+        with override_settings(ENCRYPT_CIPHER_TYPE=legacy_encrypt_type):
+            unencrypted_dict: Dict[Any, Tuple[Any, Dict[str, str]]] = {}
 
-        for model_class in app_models:
-            instance, unencryped_field_dict = generate_fernet_encrypted_instance(model_class)
-            unencrypted_dict[model_class] = (instance.pk, unencryped_field_dict)
+            for model_class in app_models:
+                instance, unencryped_field_dict = generate_encrypted_instance(model_class)
+                unencrypted_dict[model_class] = (instance.pk, unencryped_field_dict)
 
-        return unencrypted_dict
+            return unencrypted_dict
 
-    def test_migration_command(self, model_name, app_models, command_name, generate_fernet_encrypted_instances):
+    @pytest.mark.parametrize(
+        "legacy_encrypt_type, encrypt_type", [('FernetCipher', 'SM4CTR'), ('SM4CTR', 'FernetCipher')]
+    )
+    def test_migration_command(
+        self,
+        model_name,
+        app_models,
+        command_name,
+        legacy_encrypt_type,
+        encrypt_type,
+        generate_legacy_encrypted_instances,
+    ):
         """
         测试 migration_command
         分为两种情况:
@@ -102,34 +112,53 @@ class BaseTestEnctrypMigrationCmd:
         :param model_class: 实例类
         :param app_models: app 下需要考虑的 models
         :param command_name: 对应的 migration django command
-        :param unencrypted_dict:未加密实例字典，存储了每个 model 的索引(pk)和
+        :param unencrypted_dict:未加密实例字典，存储了每个 model 的索引(pk)和未加密内容
+        :param legacy_encrypt_type:存量数据的加密类型
+        :param encrypt_type:现设置的加密类型
         """
-        unencrypted_dict = generate_fernet_encrypted_instances
-        # 验证已经被fernet算法成功加密
+        legacy_encrypt_header = get_encrypt_header(legacy_encrypt_type)
+        encrypt_header = get_encrypt_header(encrypt_type)
+
+        unencrypted_dict = generate_legacy_encrypted_instances
+
+        # 验证数据已被加密
         for model_class in app_models:
             pk = unencrypted_dict[model_class][0]
             unencryped_field_dict = unencrypted_dict[model_class][1]
-            # 验证加密算法没有迁移，并且加解密成功
-            assert_fields_encryped_fernet(model_class=model_class, pk=pk, unencryped_field_dict=unencryped_field_dict)
+            assert_fields_encryped(
+                model_class=model_class,
+                pk=pk,
+                unencryped_field_dict=unencryped_field_dict,
+                encrypt_header=legacy_encrypt_header,
+            )
 
-        # 运行
-        call_command(command_name, '--no-dry-run', '--model', model_name)
+        with override_settings(ENCRYPT_CIPHER_TYPE=encrypt_type):
+            call_command(command_name, '--no-dry-run', '--model', model_name)
 
-        for model_class in app_models:
-            pk = unencrypted_dict[model_class][0]
-            unencryped_field_dict = unencrypted_dict[model_class][1]
-            if not model_name:
-                # 验证加密算法迁移为SM4,并且加解密成功
-                assert_fields_encryped_with_sm4(
-                    model_class=model_class, pk=pk, unencryped_field_dict=unencryped_field_dict
-                )
-            elif model_class.__name__ == model_name:
-                # 验证加密算法迁移为SM4,并且加解密成功
-                assert_fields_encryped_with_sm4(
-                    model_class=model_class, pk=pk, unencryped_field_dict=unencryped_field_dict
-                )
-            else:
-                # 验证加密算法没有迁移，并且加解密成功
-                assert_fields_encryped_fernet(
-                    model_class=model_class, pk=pk, unencryped_field_dict=unencryped_field_dict
-                )
+            for model_class in app_models:
+                pk = unencrypted_dict[model_class][0]
+                unencryped_field_dict = unencrypted_dict[model_class][1]
+                if not model_name:
+                    # 验证加密算法迁移完成,并且加解密成功
+                    assert_fields_encryped(
+                        model_class=model_class,
+                        pk=pk,
+                        unencryped_field_dict=unencryped_field_dict,
+                        encrypt_header=encrypt_header,
+                    )
+                elif model_class.__name__ == model_name:
+                    # 验证加密算法迁移完成,并且加解密成功
+                    assert_fields_encryped(
+                        model_class=model_class,
+                        pk=pk,
+                        unencryped_field_dict=unencryped_field_dict,
+                        encrypt_header=encrypt_header,
+                    )
+                else:
+                    # 验证加密算法没有迁移，并且加解密成功
+                    assert_fields_encryped(
+                        model_class=model_class,
+                        pk=pk,
+                        unencryped_field_dict=unencryped_field_dict,
+                        encrypt_header=legacy_encrypt_header,
+                    )

986288ecb8d04f3112ae74b385aadb0b2da28efe
shabbywu <shabbywu@tencent.com>
2023-08-30 10:45:38 +0800

    add labels to default image

--- apiserver/init.sh
+++ apiserver/init.sh
@@ -206,7 +206,7 @@ ensure-buleking-image() {
     --display_name_en "Blueking Basic Image" \
     --description_zh_cn "基于 Ubuntu，支持多构建工具组合构建" \
     --description_en "Ubuntu-based, multi-buildpack combination build support" \
-    --label secureEncrypted=1
+    --label secureEncrypted=1 normal_app=1 smart_app=1
     python manage.py bind_buildpacks --image "${image_name}" --buildpack-name "${apt_buildpack_name}"
     python manage.py bind_buildpacks --image "${image_name}" --buildpack-name "${python_buildpack_name}"
     python manage.py bind_buildpacks --image "${image_name}" --buildpack-name "${nodejs_buildpack_name}"

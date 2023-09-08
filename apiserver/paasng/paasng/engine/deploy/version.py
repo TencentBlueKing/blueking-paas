74da23287ee403d4714aef579cda4f80fadd2126
Wu jingtao <shabbywu@qq.com>
2023-09-08 14:36:04 +0800

    fix: fix get_env_deployed_version_info always return None; enhance: ImageParser support v1alpha1

--- apiserver/paasng/paasng/engine/deploy/version.py
+++ apiserver/paasng/paasng/engine/deploy/version.py
@@ -20,8 +20,8 @@ import logging
 from typing import Optional, Tuple
 
 from paas_wl.cnative.specs.crd.bk_app import BkAppResource
+from paas_wl.cnative.specs.image_parser import ImageParser
 from paas_wl.cnative.specs.models import AppModelRevision
-from paas_wl.cnative.specs.procs import parse_image
 from paasng.dev_resources.sourcectl.models import VersionInfo
 from paasng.engine.constants import RuntimeType
 from paasng.engine.deploy.archive import OfflineManager
@@ -46,15 +46,14 @@ def get_env_deployed_version_info(env: ModuleEnvironment) -> Tuple[RuntimeType, 
         logger.debug("Module: %s Env: %s is not deployed", env.module.name, env.environment)
         return build_config.build_method, None
 
-    if OfflineManager(env).has_been_offline(deployment):
+    if OfflineManager(env).has_been_offline(deployment)[1]:
         logger.debug("Module: %s Env: %s has been offline", env.module.name, env.environment)
         return build_config.build_method, None
 
     if application.type == ApplicationType.CLOUD_NATIVE and build_config.build_method == RuntimeType.CUSTOM_IMAGE:
         bkapp_revision = AppModelRevision.objects.get(pk=deployment.bkapp_revision_id)
         res = BkAppResource(**bkapp_revision.json_value)
-        image_name, image_tag = parse_image(res)
-        version_info = VersionInfo(revision="", version_name=image_tag, version_type="tag")
+        version_info = VersionInfo(revision="", version_name=ImageParser(res).get_tag() or "--", version_type="tag")
     else:
         version_info = deployment.version_info
     return build_config.build_method, version_info

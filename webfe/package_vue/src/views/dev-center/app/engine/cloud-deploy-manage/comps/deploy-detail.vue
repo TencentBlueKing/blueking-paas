d96066e3925559ee056655ec8596eb3d1594dc1c
polo <826770122@qq.com>
2023-09-12 22:54:47 +0800

    feat: 部署管理进程相关

--- webfe/package_vue/src/views/dev-center/app/engine/cloud-deploy-manage/comps/deploy-detail.vue
+++ webfe/package_vue/src/views/dev-center/app/engine/cloud-deploy-manage/comps/deploy-detail.vue
@@ -26,7 +26,7 @@
           <template slot-scope="{ row }">
             <div>
               <span>{{ row.name || '--' }}</span>
-              <span class="ml5">{{ row.success }} / {{ row.totalCount }}</span>
+              <span class="ml5">{{ row.available_instance_count }} / {{ row.targetReplicas }}</span>
               <div class="rejected-count" v-if="row.failed">{{ row.failed }}</div>
               <div class="icon-expand" v-if="row.instances.length > 1">
                 <i
@@ -117,7 +117,10 @@
                   class="paasng-icon paasng-play-circle-shape start"
                   v-else @click="handleProcessOperation(row, 'start')"></i>
               </div>
-              <i class="paasng-icon paasng-log-2 detail mr10"></i>
+              <i
+                v-bk-tooltips="$t('进程详情')"
+                class="paasng-icon paasng-log-2 detail mr10"
+                @click="showProcessDetailDialog(row)"></i>
               <bk-popover
                 theme="light"
                 ext-cls="more-operations"
@@ -132,12 +135,141 @@
         </bk-table-column>
       </bk-table>
     </section>
+
+    <bk-sideslider
+      :width="750"
+      :is-show.sync="chartSlider.isShow"
+      :title="chartSlider.title"
+      :quick-close="true"
+      :before-close="handleChartBeforeClose"
+      @hidden="handlerChartHide"
+    >
+      <div
+        slot="content"
+        class="p0 chart-wrapper"
+      >
+        <div
+          v-if="curAppInfo.feature.RESOURCE_METRICS"
+          v-bk-clickoutside="hideDatePicker"
+          class="action-box"
+        >
+          <bk-form
+            form-type="inline"
+            style="float: right;"
+          >
+            <bk-date-picker
+              v-model="initDateTimeRange"
+              style="margin-top: 4px;"
+              :shortcuts="dateShortCut"
+              :shortcuts-type="'relative'"
+              :format="'yyyy-MM-dd HH:mm:ss'"
+              :placement="'bottom-end'"
+              :placeholder="$t('选择日期时间范围')"
+              :shortcut-close="true"
+              :type="'datetimerange'"
+              :options="datePickerOption"
+              :open="isDatePickerOpen"
+              @change="handlerChange"
+              @pick-success="handlerPickSuccess"
+            >
+              <div
+                slot="trigger"
+                style="width: 310px; height: 28px;"
+                @click="toggleDatePicker"
+              >
+                <button class="action-btn timer fr">
+                  <i class="left-icon paasng-icon paasng-clock f16" />
+                  <span class="text">{{ $t(timerDisplay) }}</span>
+                  <i class="right-icon paasng-icon paasng-down-shape f12" />
+                </button>
+              </div>
+            </bk-date-picker>
+          </bk-form>
+        </div>
+        <div
+          v-if="curAppInfo.feature.RESOURCE_METRICS"
+          class="chart-box"
+        >
+          <strong class="title"> {{ $t('CPU使用') }} <span class="sub-title"> {{ $t('（单位：核）') }} </span></strong>
+          <chart
+            ref="cpuLine"
+            :options="cpuLine"
+            auto-resize
+            style="width: 750px; height: 300px; background: #1e1e21;"
+          />
+        </div>
+        <div
+          v-if="curAppInfo.feature.RESOURCE_METRICS"
+          class="chart-box"
+        >
+          <strong class="title"> {{ $t('内存使用') }} <span class="sub-title"> {{ $t('（单位：MB）') }} </span></strong>
+          <chart
+            ref="memoryLine"
+            :options="memoryLine"
+            auto-resize
+            style="width: 750px; height: 300px; background: #1e1e21;"
+          />
+        </div>
+        <div class="slider-detail-wrapper">
+          <label class="title"> {{ $t('详细信息') }} </label>
+          <section class="detail-item">
+            <label class="label"> {{ $t('类型：') }} </label>
+            <div class="content">
+              {{ processPlan.processType }}
+            </div>
+          </section>
+          <section class="detail-item">
+            <label class="label"> {{ $t('实例数上限：') }} </label>
+            <div class="content">
+              {{ processPlan.maxReplicas }}
+            </div>
+          </section>
+          <section class="detail-item">
+            <label class="label"> {{ $t('单实例资源配额：') }} </label>
+            <div class="content">
+              {{ $t('内存:') }} {{ processPlan.memLimit }} / CPU: {{ processPlan.cpuLimit }}
+            </div>
+          </section>
+          <section class="detail-item">
+            <label class="label"> {{ $t('进程间访问链接：') }} </label>
+            <div class="content">
+              {{ processPlan.clusterLink }}
+            </div>
+          </section>
+          <p style="padding-left: 112px; margin-top: 5px; color: #c4c6cc;">
+            {{ $t('注意：进程间访问链接地址只能用于同集群内的不同进程间通信，可在 “模块管理” 页面查看进程的集群信息。更多进程间通信的说明。请参考') }} <a
+              target="_blank"
+              :href="GLOBAL.DOC.PROCESS_SERVICE"
+            > {{ $t('进程间通信') }} </a>
+          </p>
+        </div>
+      </div>
+    </bk-sideslider>
   </div>
 </template>
 
 <script>
 import moment from 'moment';
+import appBaseMixin from '@/mixins/app-base-mixin';
+import sidebarDiffMixin from '@/mixins/sidebar-diff-mixin';
+import chartOption from '@/json/instance-chart-option';
+import ECharts from 'vue-echarts/components/ECharts.vue';
+
+// let maxReplicasNum = 0;
+
+const initEndDate = moment().format('YYYY-MM-DD HH:mm:ss');
+const initStartDate = moment().subtract(1, 'hours')
+  .format('YYYY-MM-DD HH:mm:ss');
+let timeRangeCache = '';
+let timeShortCutText = '';
 export default {
+  components: {
+    // dropdown,
+    // tooltipConfirm,
+    // numInput,
+    chart: ECharts,
+  },
+  mixins: [appBaseMixin, sidebarDiffMixin],
   props: {
     deploymentInfo: {
       type: Object,
@@ -145,11 +277,132 @@ export default {
     },
   },
   data() {
+    const dateShortCut = [
+      {
+        text: this.$t('最近5分钟'),
+        value() {
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 60 * 1000 * 5);
+          return [start, end];
+        },
+        onClick() {
+          timeRangeCache = '5m';
+          timeShortCutText = this.$t('最近5分钟');
+        },
+      },
+      {
+        text: this.$t('最近1小时'),
+        value() {
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 1);
+          return [start, end];
+        },
+        onClick() {
+          timeRangeCache = '1h';
+          timeShortCutText = this.$t('最近1小时');
+        },
+      },
+      {
+        text: this.$t('最近3小时'),
+        value() {
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 3);
+          return [start, end];
+        },
+        onClick() {
+          timeRangeCache = '3h';
+          timeShortCutText = this.$t('最近3小时');
+        },
+      },
+      {
+        text: this.$t('最近12小时'),
+        value() {
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 12);
+          return [start, end];
+        },
+        onClick() {
+          timeRangeCache = '12h';
+          timeShortCutText = this.$t('最近12小时');
+        },
+      },
+      {
+        text: this.$t('最近1天'),
+        value() {
+          const end = new Date();
+          const start = new Date();
+          start.setTime(start.getTime() - 3600 * 1000 * 24);
+          return [start, end];
+        },
+        onClick() {
+          timeRangeCache = '1d';
+          timeShortCutText = this.$t('最近1天');
+        },
+      },
+    ];
     return {
       isTableLoading: false,
       isColumnExpand: true,
       deployData: {},
       allProcesses: [],
+      chartSlider: {
+        isShow: false,
+        title: '',
+      },
+      curChartTimeRange: '1h',
+      curLogTimeRange: '1h',
+      chartRangeList: [
+        {
+          id: '1h',
+          name: this.$t('最近1小时'),
+        },
+        {
+          id: '6h',
+          name: this.$t('最近6小时'),
+        },
+        {
+          id: '12h',
+          name: this.$t('最近12小时'),
+        },
+        {
+          id: '1d',
+          name: this.$t('最近24小时'),
+        },
+      ],
+      currentClickObj: {
+        operateIconTitle: '',
+        index: 0,
+      },
+      prevProcessVersion: 0,
+      prevInstanceVersion: 0,
+      serverTimeout: 30,
+      cloudServerEvent: null,
+
+      timerDisplay: this.$t('最近1小时'),
+      datePickerOption: {
+        // 小于今天的都不能选
+        disabledDate(date) {
+          return date && date.valueOf() > Date.now() - 86400;
+        },
+      },
+      dateParams: {
+        start_time: initStartDate,
+        end_time: initEndDate,
+      },
+      dateShortCut,
+      initDateTimeRange: [initStartDate, initEndDate],
+      isDatePickerOpen: false,
+      cpuLine: chartOption.cpu,
+      memoryLine: chartOption.memory,
+      processPlan: {
+        processType: 'unkonwn',
+        targetReplicas: 0,
+        maxReplicas: 0,
+      },
     };
   },
 
@@ -170,6 +423,7 @@ export default {
       row.isExpand = !row.isExpand;
     },
     handleProcessOperation(row, type) {
+      console.log(row);
       row.status = type;
     },
     handleExpansionAndContraction() {
@@ -235,7 +489,10 @@ export default {
           available_instance_count: processInfo.success,
           failed: processInfo.failed,
           resourceLimit: processInfo.resource_limit,
+          cpuLimit: processInfo.cpu_limit,
+          memLimit: processInfo.memory_limit,
           clusterLink: processInfo.cluster_link,
+          type,
         };
 
         // 日期转换
@@ -247,6 +504,255 @@ export default {
       });
       this.allProcesses = JSON.parse(JSON.stringify(allProcesses));
       console.log('this.allProcesses', this.allProcesses);
+    },
+
+    // 进程详情
+    showProcessDetailDialog(process) {
+      console.log('process', process);
+      this.processPlan = {
+        replicas: process.instance,
+        processType: process.type,
+        targetReplicas: process.targetReplicas,
+        maxReplicas: process.maxReplicas,
+        status: process.status,
+        cpuLimit: this.transfer_cpu_unit(process.cpuLimit),
+        memLimit: process.memLimit,
+        clusterLink: process.clusterLink,
+      };
+      this.curProcess = process;
+      this.curProcessKey = process.name;
+      this.chartSlider.title = `${this.$t('进程')} ${process.name}${this.$t('详情')}`;
+      this.chartSlider.isShow = true;
+      if (this.curAppInfo.feature.RESOURCE_METRICS) {
+        this.getInstanceChart(process);
+      }
+    },
+
+    transfer_cpu_unit(cpuLimit) {
+      if (cpuLimit.endsWith('m')) {
+        cpuLimit = parseInt(/^\d+/.exec(cpuLimit)) / 1000;
+      }
+      return cpuLimit + this.$t('核');
+    },
+
+    /**
+     * 显示实例指标数据
+     */
+    getInstanceChart(processes) {
+      this.$nextTick(() => {
+        const cpuRef = this.$refs.cpuLine;
+        const memRef = this.$refs.memoryLine;
+
+        cpuRef && cpuRef.mergeOptions({
+          xAxis: [
+            {
+              data: [],
+            },
+          ],
+          series: [],
+        });
+
+        memRef && memRef.mergeOptions({
+          xAxis: [
+            {
+              data: [],
+            },
+          ],
+          series: [],
+        });
+
+        cpuRef && cpuRef.showLoading({
+          text: this.$t('正在加载'),
+          color: '#30d878',
+          textColor: '#fff',
+          maskColor: 'rgba(255, 255, 255, 0.8)',
+        });
+
+        memRef && memRef.showLoading({
+          text: this.$t('正在加载'),
+          color: '#30d878',
+          textColor: '#fff',
+          maskColor: 'rgba(255, 255, 255, 0.8)',
+        });
+
+        this.fetchMetric({
+          cpuRef,
+          memRef,
+          processes,
+        });
+      });
+    },
+
+    async handleBeforeClose() {
+      return this.$isSidebarClosed(JSON.stringify(this.curLogTimeRange));
+    },
+
+    async handleChartBeforeClose() {
+      const time = this.initDateTimeRange.map(time => moment(time).format('YYYY-MM-DD HH:mm:ss'));
+      return this.$isSidebarClosed(JSON.stringify(time));
+    },
+
+    /**
+     * 图标侧栏隐藏回调处理
+     */
+    handlerChartHide() {
+      this.dateParams = Object.assign({}, {
+        start_time: initStartDate,
+        end_time: initEndDate,
+      });
+      this.initDateTimeRange = [initStartDate, initEndDate];
+      this.isDatePickerOpen = false;
+      this.clearChart();
+    },
+
+    hideDatePicker() {
+      this.isDatePickerOpen = false;
+    },
+
+    handlerChange(dates) {
+      this.dateParams.start_time = dates[0];
+      this.dateParams.end_time = dates[1];
+      this.dateParams.time_range = timeRangeCache || 'customized';
+      if (timeShortCutText) {
+        this.timerDisplay = timeShortCutText;
+      } else {
+        this.timerDisplay = `${dates[0]} - ${dates[1]}`;
+      }
+      this.isDateChange = true;
+      timeShortCutText = ''; // 清空
+      timeRangeCache = ''; // 清空
+    },
+
+    handlerPickSuccess() {
+      this.isDatePickerOpen = false;
+      if (this.isDateChange) {
+        this.getInstanceChart(this.curProcess);
+        this.isDateChange = false;
+      }
+    },
+
+    toggleDatePicker() {
+      this.isDatePickerOpen = !this.isDatePickerOpen;
+    },
+
+
+    async fetchMetric(conf) {
+      // 请求数据
+      const fetchData = (type, processType) => {
+        const params = {
+          appCode: this.appCode,
+          moduleId: this.curModuleId,
+          env: this.environment,
+          metric_type: type,
+          process_type: processType,
+          start_time: this.dateParams.start_time,
+          end_time: this.dateParams.end_time,
+        };
+        return this.$store.dispatch('processes/getInstanceMetrics', params);
+      };
+      // 数据处理
+      const getData = (payload) => {
+        const datas = [];
+        let limitDatas = null;
+        payload.result.forEach((instance) => {
+          const instanceName = instance.display_name;
+          instance.results.forEach((item) => {
+            const dataList = item.results;
+
+            if (item.type_name === 'cpu') {
+              dataList.forEach((data) => {
+                if (data.type_name === 'current') {
+                  data.display_name = `${instanceName}-${data.display_name}`;
+                  datas.push(data);
+                } else if (data.type_name === 'limit') {
+                  limitDatas = data;
+                }
+              });
+            } else {
+              dataList.forEach((data) => {
+                if (data.type_name === 'current') {
+                  data.display_name = `${instanceName}-${data.display_name}`;
+                  datas.push(data);
+                } else if (data.type_name === 'limit') {
+                  limitDatas = data;
+                }
+              });
+            }
+          });
+        });
+        limitDatas && (datas.unshift(limitDatas));
+        return datas;
+      };
+      try {
+        const res = await Promise.all([fetchData('cpu', conf.processes.type), fetchData('mem', conf.processes.type)]);
+        const [res1, res2] = res;
+        const cpuData = getData(res1);
+        const memData = getData(res2);
+        this.renderChartNew(cpuData, 'cpu', conf.cpuRef);
+        this.renderChartNew(memData, 'mem', conf.memRef);
+      } catch (e) {
+        this.$paasMessage({
+          theme: 'error',
+          message: e.message,
+        });
+        this.clearChart();
+      } finally {
+        this.isChartLoading = false;
+        conf.cpuRef.hideLoading();
+        conf.memRef.hideLoading();
+      }
+    },
+
+    /**
+      * 清空图表数据
+    */
+    clearChart() {
+      const cpuRef = this.$refs.cpuLine;
+      const memRef = this.$refs.memoryLine;
+
+      cpuRef && cpuRef.mergeOptions({
+        xAxis: [
+          {
+            data: [],
+          },
+        ],
+        series: [
+          {
+            name: '',
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            areaStyle: {
+              normal: {
+                opacity: 0,
+              },
+            },
+            data: [0],
+          },
+        ],
+      });
+
+      memRef && memRef.mergeOptions({
+        xAxis: [
+          {
+            data: [],
+          },
+        ],
+        series: [
+          {
+            name: '',
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            areaStyle: {
+              normal: {
+                opacity: 0,
+              },
+            },
+            data: [0],
+          },
+        ],
+      });
     },
   },
 };

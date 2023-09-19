3be326abd2b63b8d19a6d60e86799d3d8a777adb
shabbywu <shabbywu@tencent.com>
2023-09-19 10:33:41 +0800

    minor: 修改名称

--- apiserver/paasng/paasng/engine/constants.py
+++ apiserver/paasng/paasng/engine/constants.py
@@ -24,7 +24,7 @@ from django.utils.translation import gettext_lazy as _
 from paasng.utils.basic import ChoicesEnum
 
 PROC_DEFAULT_REPLICAS = 1
-DOCKER_BUILD_STEPSET_NAME = "dockerbuild"
+DOCKER_BUILD_STEPSET_NAME = "docker-build"
 IMAGE_RELEASE_STEPSET_NAME = "image-release"
 
 

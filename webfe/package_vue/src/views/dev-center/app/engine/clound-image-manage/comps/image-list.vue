12a9d37aab1fa01f5518ad1f8be227c74234d09f
leafage-collb <3303710797@qq.com>
2023-10-11 14:55:09 +0800

    feat: 镜像管理->镜像列表、构建历史功能开发

---
+++ webfe/package_vue/src/views/dev-center/app/engine/clound-image-manage/comps/image-list.vue
@@ -0,0 +1,397 @@
+<template>
+  <div class="clund-image-list">
+    <section class="top-action-bar">
+      <div class="right">
+        <bk-input
+          class="ml10"
+          style="width: 320px"
+          v-model="searchValue"
+          right-icon="paasng-icon paasng-search"
+          @enter="handleSearch"
+        ></bk-input>
+        <bk-select
+          v-model="moduleName"
+          style="width: 150px"
+          :clearable="false"
+          @selected="handleChangeModule"
+        >
+          <bk-option
+            v-for="option in curAppModuleList"
+            :key="option.name"
+            :id="option.name"
+            :name="option.name"
+          ></bk-option>
+        </bk-select>
+      </div>
+    </section>
+    <div class="table-wrapper mt15">
+      <bk-table
+        v-bkloading="{ isLoading: isTableLoading }"
+        ext-cls="image-table-cls"
+        :data="imageList"
+        :outer-border="false"
+        :size="'small'"
+        :pagination="pagination"
+        :height="imageList.length ? '' : '520px'"
+        :show-overflow-tooltip="true"
+        @page-change="handlePageChange"
+        @page-limit-change="handlePageLimitChange"
+        @expand-change="handleExpandChange"
+      >
+        <div slot="empty">
+          <table-empty
+            v-if="searchValue"
+            :keyword="tableEmptyConf.keyword"
+            :abnormal="tableEmptyConf.isAbnormal"
+            @reacquire="getImageList"
+            @clear-filter="handleClearFilter"
+          />
+          <table-empty
+            v-else
+            :explanation="$t('该应用下所有模块都为提供源码在开发者中心构建镜像')"
+            empty
+          />
+        </div>
+        <bk-table-column
+          type="expand"
+          width="30"
+          class="expand-image"
+        >
+          <template slot-scope="{ row }">
+            <div
+              class="image-info"
+              v-bkloading="{ isLoading: row.isLoading }"
+            >
+              <section class="left-info">
+                <bk-form :model="row.detail">
+                  <bk-form-item :label="`${$t('镜像仓库')}：`">
+                    <span class="form-text">{{ row.detail.image_info.repository || '--' }}</span>
+                  </bk-form-item>
+                  <bk-form-item :label="`${$t('镜像 Tag')}：`">
+                    <span class="form-text">{{ row.detail.image_info.tag || '--' }}</span>
+                  </bk-form-item>
+                  <bk-form-item :label="`${$t('大小')}：`">
+                    <span class="form-text">{{ row.detail.image_info.size || '--' }}</span>
+                  </bk-form-item>
+                  <bk-form-item :label="`${$t('更新时间')}：`">
+                    <span class="form-text">{{ row.detail.image_info.updated || '--' }}</span>
+                  </bk-form-item>
+                  <bk-form-item :label="`${$t('摘要')}：`">
+                    <span class="form-text">{{ row.detail.image_info.digest || '--' }}</span>
+                  </bk-form-item>
+                  <bk-form-item :label="`${$t('备注')}：`">
+                    <span class="form-text">{{ row.detail.image_info.invoke_message || '--' }}</span>
+                  </bk-form-item>
+                </bk-form>
+              </section>
+              <div
+                class="fright-middle fright-last"
+              >
+                <h3> {{ $t('部署记录') }} </h3>
+                <ul class="dynamic-list">
+                  <template v-if="row.detail.deploy_records?.length">
+                    <li
+                      v-for="(item, itemIndex) in row.detail.deploy_records"
+                      :key="itemIndex"
+                    >
+                      <p class="dynamic-content">
+                        {{ item.operator }} {{ $t('部署到') }}{{item.environment === 'stag' ? $t('预发布环境') : $t('生产环境')}}
+                        <span class="dynamic-time">{{ item.at }}</span>
+                      </p>
+                    </li>
+                    <li />
+                  </template>
+                  <template v-else>
+                    <table-empty empty />
+                  </template>
+                </ul>
+              </div>
+            </div>
+          </template>
+        </bk-table-column>
+        <bk-table-column
+          :label="$t('镜像 tag')"
+          prop="tag"
+          :show-overflow-tooltip="true"
+        />
+        <bk-table-column
+          :label="$t('大小（MB）')"
+          prop="size"
+          sortable
+        />
+        <bk-table-column
+          :label="$t('更新时间')"
+          prop="updated"
+          :show-overflow-tooltip="true"
+        />
+        <bk-table-column
+          :label="$t('摘要')"
+          prop="digest"
+          :show-overflow-tooltip="true"
+        />
+      </bk-table>
+    </div>
+  </div>
+</template>
+
+<script>
+import appBaseMixin from '@/mixins/app-base-mixin';
+export default {
+  name: 'ClundImageList',
+  mixins: [appBaseMixin],
+  data() {
+    return {
+      moduleName: '',
+      searchValue: '',
+      isTableLoading: false,
+      imageList: [],
+      pagination: {
+        current: 1,
+        count: 0,
+        limit: 10,
+      },
+      tableEmptyConf: {
+        keyword: '',
+        isAbnormal: false,
+      },
+    };
+  },
+  watch: {
+    searchValue(val) {
+      if (!val) {
+        this.getImageList();
+      }
+    },
+    $route() {
+      this.init();
+    },
+  },
+  created() {
+    this.init();
+  },
+  methods: {
+    init() {
+      this.moduleName = this.curModuleId;
+      this.getImageList();
+    },
+    /**
+     * 切换模块
+     */
+    handleChangeModule() {
+      this.getImageList();
+    },
+    /**
+     * 获取镜像列表
+     */
+    async getImageList(page = 1) {
+      const curPage = page || this.pagination.current;
+
+      this.isTableLoading = true;
+      try {
+        const res = await this.$store.dispatch('imageManage/getImageList', {
+          appCode: this.appCode,
+          moduleName: this.moduleName,
+          pageParams: {
+            limit: this.pagination.limit,
+            offset: this.pagination.limit * (curPage - 1),
+            search_term: this.searchValue,
+          },
+        });
+        this.imageList = res.results.map(image => ({
+          isLoading: false,
+          detail: {
+            build_records: [],
+            deploy_records: [],
+            image_info: {},
+          },
+          ...image,
+        }));
+        this.pagination.count = res.count;
+        this.updateTableEmptyConfig();
+        this.tableEmptyConf.isAbnormal = false;
+      } catch (e) {
+        this.tableEmptyConf.isAbnormal = true;
+        this.$paasMessage({
+          theme: 'error',
+          message: e.detail || e.message || this.$t('接口异常'),
+        });
+      } finally {
+        this.isTableLoading = false;
+      }
+    },
+
+    /**
+     * 获取镜像详情
+     */
+    async getImageDetail(id) {
+      try {
+        const res = await this.$store.dispatch('imageManage/getImageDetail', {
+          appCode: this.appCode,
+          moduleName: this.moduleName,
+          buildId: id,
+        });
+        this.imageList.forEach((image) => {
+          if (image.id === id) {
+            image.detail = res;
+          }
+        });
+      } catch (e) {
+        this.$paasMessage({
+          theme: 'error',
+          message: e.detail || e.message || this.$t('接口异常'),
+        });
+      } finally {
+        this.imageList.forEach((image) => {
+          if (image.id === id) {
+            image.isLoading = false;
+          }
+        });
+      }
+    },
+
+    /** 搜索 */
+    handleSearch() {
+      this.getImageList();
+    },
+
+    /** 页码改变 */
+    handlePageChange(newPage) {
+      this.pagination.current = newPage;
+      this.getImageList(newPage);
+    },
+
+    /** 页容量改变 */
+    handlePageLimitChange(limit) {
+      this.pagination.limit = limit;
+      this.pagination.current = 1;
+      this.getImageList(this.pagination.current);
+    },
+
+    /** 当前行展开事件 */
+    handleExpandChange(row) {
+      // 如果存在数据则不请求
+      if (Object.keys(row.detail.image_info).length) {
+        return;
+      }
+      row.isLoading = true;
+      this.getImageDetail(row.id);
+    },
+
+    /** 清空筛选条件 */
+    handleClearFilter() {
+      this.searchValue = '';
+    },
+
+    updateTableEmptyConfig() {
+      if (this.searchValue) {
+        this.tableEmptyConf.keyword = 'placeholder';
+        return;
+      }
+      this.tableEmptyConf.keyword = '';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.clund-image-list {
+  padding: 24px;
+
+  .top-action-bar {
+    .right {
+      display: flex;
+      flex-direction: row-reverse;
+    }
+  }
+
+  .image-info {
+    display: flex;
+    justify-content: space-between;
+    font-size: 12px;
+    padding: 10px 24px;
+
+    .left-info {
+      /deep/ .bk-form .bk-label {
+        font-size: 12px;
+        padding-right: 12px;
+        color: #63656e;
+      }
+      .form-text {
+        color: #313238;
+      }
+      /deep/ .bk-form-item + .bk-form-item {
+        margin-top: 0;
+      }
+    }
+  }
+
+  .image-table-cls /deep/ .bk-table-body td.bk-table-expanded-cell {
+    background: #f5f7fa;
+    padding: 0;
+  }
+
+  .fright-last{
+    padding: 16px;
+    background: #fafcfe;
+    border: 1px solid #DCDEE5;
+    min-width: 254px;
+    h3 {
+      font-weight: 700;
+      font-size: 12px;
+      color: #63656E;
+      height: 32px;
+      line-height: 32px;
+    }
+  }
+  .dynamic-list li {
+    padding-bottom: 5px;
+    padding-left: 20px;
+    position: relative;
+  }
+  .dynamic-list li:before {
+    position: absolute;
+    content: "";
+    width: 10px;
+    height: 10px;
+    top: 3px;
+    left: 1px;
+    border: solid 1px rgba(87, 163, 241, 1);
+    border-radius: 50%;
+  }
+  .dynamic-list li:after {
+    position: absolute;
+    content: "";
+    width: 1px;
+    height: 40px;
+    top: 15px;
+    left: 6px;
+  }
+  .dynamic-list li:before {
+    border: solid 2px #D8D8D8;
+  }
+  .dynamic-list li:after {
+    background: #D8D8D8;
+  }
+  .dynamic-list li:last-child:before {
+    border: solid 2px #D8D8D8;
+  }
+  .dynamic-list li:last-child:after {
+    background: transparent;
+  }
+  .dynamic-time {
+    line-height: 18px;
+    font-size: 12px;
+    color: #979BA5;
+    cursor: default;
+    margin-top: 3px;
+  }
+  .dynamic-content {
+    display: flex;
+    flex-direction: column;
+    line-height: 24px;
+    height: 48px;
+    overflow: hidden;
+    color: #63656e;
+  }
+}
+</style>
---
+++ webfe/package_vue/src/views/dev-center/app/engine/clound-image-manage/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="image-manage">
+    <cloud-app-top-bar
+      :title="$t('镜像管理')"
+      :active="active"
+      :nav-list="panels"
+      :module-id="curModuleId"
+      @change="handleTabChange"
+    />
+    <div class="app-container middle main">
+      <router-view :key="routeIndex"></router-view>
+    </div>
+  </div>
+</template>
+
+<script>
+import cloudAppTopBar from '@/components/cloud-app-top-bar.vue';
+import appBaseMixin from '@/mixins/app-base-mixin';
+export default {
+  name: 'ClundImageManage',
+  components: {
+    cloudAppTopBar,
+  },
+  mixins: [appBaseMixin],
+  data() {
+    return {
+      active: 'image',
+      routeIndex: 0,
+      panels: [
+        { name: 'image', label: this.$t('镜像管理'), routeName: 'cloudAppImageList' },
+        { name: 'history', label: this.$t('构建历史'), routeName: 'cloudAppBuildHistory' },
+      ],
+    };
+  },
+  created() {
+    this.active = this.panels.find(item => item.routeName === this.$route.name).name || 'image';
+  },
+  methods: {
+    handleTabChange(name) {
+      this.active = name;
+      const curPanel = this.panels.find(item => item.name === name);
+      this.$router.push({
+        name: curPanel.routeName,
+        params: {
+          id: this.curAppCode,
+        },
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.image-manage {
+  .main {
+    padding-top: 0;
+    margin-top: 16px;
+    background: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 4px 0 #1919290d;
+  }
+}
+</style>

35415b98ca77db63a6c68c5395203fcd6a5ccde9
shabbywu <shabbywu@tencent.com>
2023-10-19 11:30:24 +0800

    feature: 提供 ModuleProcessSpec 模型的查询和保存接口; 增加 ProcessSpecEnvOverlay 存储环境相关的配置

--- apiserver/paasng/paasng/platform/bkapp_model/serializers.py
+++ apiserver/paasng/paasng/platform/bkapp_model/serializers.py
@@ -15,10 +15,45 @@ limitations under the License.
 We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
+from django.conf import settings
 from rest_framework import serializers
+
+from paas_wl.bk_app.processes.drf_serializers import ScalingConfigSLZ
 
 
 class GetManifestInputSLZ(serializers.Serializer):
     output_format = serializers.ChoiceField(
         help_text='The output format', choices=['json', 'yaml'], required=False, default='json'
     )
+
+
+class ProcessSpecEnvOverlaySLZ(serializers.Serializer):
+    """进程配置-环境相关配置"""
+
+    environment_name = serializers.CharField(help_text="环境名称")
+
+    plan_name = serializers.CharField(help_text="资源配额方案", required=False)
+    target_replicas = serializers.IntegerField(help_text="副本数量(手动调节)", min_value=0, required=False)
+    autoscaling = serializers.BooleanField(help_text="是否启用自动扩缩容", required=False, default=False)
+    scaling_config = ScalingConfigSLZ(help_text="自动扩缩容配置", required=False)
+
+
+class ModuleProcessSpecMetadataSLZ(serializers.Serializer):
+    """特性开关"""
+
+    allow_set_image = serializers.BooleanField(default=False, help_text="是否允许编辑 image 字段")
+
+
+class ModuleProcessSpecSLZ(serializers.Serializer):
+    """进程配置"""
+
+    name = serializers.CharField(help_text="进程名称")
+
+    image = serializers.CharField(help_text="镜像仓库/镜像地址", allow_null=True, required=False)
+    image_credential_name = serializers.CharField(help_text="镜像凭证", allow_null=True, required=False)
+    command = serializers.ListSerializer(child=serializers.CharField(), help_text="启动命令", default=list)
+    args = serializers.ListSerializer(child=serializers.CharField(), help_text="命令参数", default=list)
+    port = serializers.IntegerField(help_text="容器端口", min_value=1, max_value=65535, default=settings.CONTAINER_PORT)
+    env_overlay = serializers.DictField(child=ProcessSpecEnvOverlaySLZ(), help_text="环境相关配置", required=False)
+
+    metadata = ModuleProcessSpecMetadataSLZ(read_only=True, help_text="辅助前端控制特性的自动")

54ccbf89147617456a5e6c34625dc4490ec32c0f
leafage-collb <3303710797@qq.com>
2023-10-23 12:16:59 +0800

    feat: markdown webpack配置

--- webfe/package_vue/bk.config.js
+++ webfe/package_vue/bk.config.js
@@ -37,6 +37,17 @@ module.exports = {
       }]);
     config.plugin('preTaskPlugin')
       .use(new PreTaskPlugin());
+    config.module
+      .rule('md')
+      .test(/\.md/)
+      .use('vue-loader')
+      .loader('vue-loader')
+      .end()
+      .use('vue-markdown-loader')
+      .loader('vue-markdown-loader/lib/markdown-compiler')
+      .options({
+        raw: true,
+      });
     return config;
   },
 };

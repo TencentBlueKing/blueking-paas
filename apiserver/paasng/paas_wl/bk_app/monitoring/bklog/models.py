2f0c6a9ecd5ba68b1ad9fa2658159b17eb0eeee0
shabbywu <shabbywu@tencent.com>
2023-10-30 17:09:19 +0800

    enhance: BKLogConfig 采集配置增加模块相关的 label selector

--- apiserver/paasng/paas_wl/bk_app/monitoring/bklog/models.py
+++ apiserver/paasng/paas_wl/bk_app/monitoring/bklog/models.py
@@ -34,15 +34,15 @@ class LabelSelector:
     label selector matches all objects. A null label selector matches
     no objects.
 
-    :param match_expressions: matchExpressions is a list of label selector requirements. The requirements are ANDed.
-    :param match_labels: matchLabels is a map of {key,value} pairs.
+    :param matchExpressions: matchExpressions is a list of label selector requirements. The requirements are ANDed.
+    :param matchLabels: matchLabels is a map of {key,value} pairs.
             A single {key,value} in the matchLabels map is equivalent to an element of matchExpressions,
             whose key field is "key", the operator is "In", and the values array contains only "value".
             The requirements are ANDed.
     """
 
-    match_expressions: List[MatchExpression] = field(default_factory=list)
-    match_labels: Dict[str, str] = field(default_factory=dict)
+    matchExpressions: List[MatchExpression] = field(default_factory=list)
+    matchLabels: Dict[str, str] = field(default_factory=dict)
 
 
 @dataclass

8a7593c9472f4f12d847cbf43cc13a6dd500c7c3
jamesgetx <gejun.coolfriend@gmail.com>
2023-11-03 11:28:58 +0800

    revert: minor
    N

--- apiserver/paasng/paas_wl/bk_app/cnative/specs/procs/quota.py
+++ apiserver/paasng/paas_wl/bk_app/cnative/specs/procs/quota.py
@@ -91,6 +91,9 @@ class ResourceQuotaReader:
         if self.res.apiVersion == ApiVersion.V1ALPHA2:
             return self.from_v1alpha2_bkapp()
 
+        if self.res.apiVersion == ApiVersion.V1ALPHA1:
+            return self.from_v1alpha1_bkapp()
+
         raise ValueError(f"Unsupported api version: {self.res.apiVersion}")
 
     def from_v1alpha2_bkapp(self) -> Dict[str, ResourceQuota]:
@@ -104,3 +107,6 @@ class ResourceQuotaReader:
         return {
             p.name: PLAN_TO_LIMIT_QUOTA_MAP[p.resQuotaPlan or ResQuotaPlan.P_DEFAULT] for p in self.res.spec.processes
         }
+
+    def from_v1alpha1_bkapp(self) -> Dict[str, ResourceQuota]:
+        return {p.name: ResourceQuota(cpu=p.cpu or '', memory=p.memory or '') for p in self.res.spec.processes}

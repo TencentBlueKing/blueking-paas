89cc1db183d1f4c682cd6519d094806888e1b923
leafage-collb <3303710797@qq.com>
2023-11-03 16:53:48 +0800

    feat: 进程服务管理接口对接

---
+++ webfe/package_vue/src/components/loader/loading/process-service.vue
@@ -0,0 +1,37 @@
+<template>
+  <content-loader
+    :style="{ 'transform': `scaleX(${contentWidth / baseWidth})`, 'margin-top': '10px' }"
+    :width="baseWidth"
+    :height="360"
+    :speed="loadingConf.speed"
+    :primary-color="loadingConf.primaryColor"
+    :secondary-color="loadingConf.secondaryColor">
+    <rect x="0" y="0" rx="2" ry="2" :width="baseWidth" height="62" />
+    <rect x="0" y="84" rx="2" ry="2" :width="baseWidth" height="62" />
+  </content-loader>
+</template>
+
+<script>
+import { ContentLoader } from 'vue-content-loader';
+export default {
+  components: {
+    ContentLoader,
+  },
+  props: {
+    baseWidth: {
+      type: Number,
+      default: 1180,
+    },
+    contentWidth: {
+      type: Number,
+      default: 1180,
+    },
+  },
+  computed: {
+    loadingConf() {
+      return this.$store.state.loadingConf;
+    },
+  },
+};
+</script>
+

b6ad45c39716786656bbcbceaea3383a53c96dc8
shabbywu <shabbywu@tencent.com>
2023-11-08 12:11:18 +0800

    minor: 优化变量赋值方式

--- operator/pkg/controllers/resources/deployment.go
+++ operator/pkg/controllers/resources/deployment.go
@@ -181,13 +181,11 @@ func buildContainers(
 // buildImagePullSecrets 返回拉取镜像的 Secrets 列表
 func buildImagePullSecrets(app *paasv1alpha2.BkApp) []corev1.LocalObjectReference {
 	var pullSecretName string
-	switch app.GetAnnotations()[paasv1alpha2.ImageCredentialsRefAnnoKey] {
+	switch pullSecretName = app.GetAnnotations()[paasv1alpha2.ImageCredentialsRefAnnoKey]; pullSecretName {
 	case "":
 		return nil
 	case "true":
 		pullSecretName = paasv1alpha2.LegacyImagePullSecretName
-	default:
-		pullSecretName = app.GetAnnotations()[paasv1alpha2.ImageCredentialsRefAnnoKey]
 	}
 	// DefaultImagePullSecretName 由 workloads 服务负责创建
 	return []corev1.LocalObjectReference{

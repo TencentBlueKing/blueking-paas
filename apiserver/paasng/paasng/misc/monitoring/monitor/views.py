8c1cf88220be77a85f881fe15b8091e08aab12fb
tiga <68499559+SheepSheepChen@users.noreply.github.com>
2023-11-16 16:02:27 +0800

    fix:告警记录，告警策略未部署时 返回空数据 (#893)

--- apiserver/paasng/paasng/misc/monitoring/monitor/views.py
+++ apiserver/paasng/paasng/misc/monitoring/monitor/views.py
@@ -255,7 +255,8 @@ class ListAlertsView(ViewSet, ApplicationCodeInPathMixin):
         try:
             alerts = make_bk_monitor_client().query_alerts(serializer.validated_data)
         except BkMonitorSpaceDoesNotExist:
-            return Response()
+            # BkMonitorSpace 不存在（应用未部署）时，返回空列表
+            return Response([])
         except BkMonitorGatewayServiceError as e:
             raise error_codes.QUERY_ALERTS_FAILED.f(str(e))
 
@@ -275,7 +276,8 @@ class ListAlarmStrategiesView(ViewSet, ApplicationCodeInPathMixin):
         try:
             alarm_strategies = make_bk_monitor_client().query_alarm_strategies(serializer.validated_data)
         except BkMonitorSpaceDoesNotExist:
-            return Response()
+            # BkMonitorSpace 不存在（应用未部署）时，返回空字典
+            return Response({})
         except BkMonitorGatewayServiceError as e:
             raise error_codes.QUERY_ALARM_STRATEGIES_FAILED.f(str(e))
 

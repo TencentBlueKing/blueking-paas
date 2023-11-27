041279cdce64483d31a98e774563a60c2c5227e1
leafage-collb <3303710797@qq.com>
2023-11-27 17:40:32 +0800

    fix: 部署阶段-左侧不展示对应详情信息修复，eslint

--- webfe/package_vue/src/views/plugin-center/plugin/version-manager/release-stages/deploy.vue
+++ webfe/package_vue/src/views/plugin-center/plugin/version-manager/release-stages/deploy.vue
@@ -83,133 +83,133 @@
   </div>
 </template>
 <script>
-    import pluginBaseMixin from '@/mixins/plugin-base-mixin';
-    import releaseTimeline from './comps/release-timeline';
-    import releaseLog from './comps/release-log.vue';
-    import stageBaseMixin from './stage-base-mixin';
+import pluginBaseMixin from '@/mixins/plugin-base-mixin';
+import releaseTimeline from './comps/release-timeline';
+import releaseLog from './comps/release-log.vue';
+import stageBaseMixin from './stage-base-mixin';
 
-    export default {
-        components: {
-            releaseTimeline,
-            releaseLog
-        },
-        mixins: [stageBaseMixin, pluginBaseMixin],
-        props: {
-            stageData: {
-                type: Object,
-                default: () => {}
-            }
-        },
-        data: function () {
-          return {
-            winHeight: 300,
-            logs: [],
-            steps: [],
-            status: ''
-          };
-        },
-        computed: {
-            releaseId () {
-                return this.$route.query.release_id;
-            }
-        },
-        watch: {
-            stageData: {
-                handler (newStageData) {
-                    this.logs = newStageData.detail.logs;
-                    this.steps = this.modifyDeployStepsData(newStageData.detail.steps);
-                    this.status = newStageData.status;
-                },
-                deep: true,
-                immediate: true
-            }
-        },
-        methods: {
-          getDisplayTime (payload) {
-                let theTime = payload;
-                if (theTime < 1) {
-                    return `< 1${this.$t('秒')}`;
-                }
-                let middle = 0;
-                let hour = 0;
+export default {
+  components: {
+    releaseTimeline,
+    releaseLog,
+  },
+  mixins: [stageBaseMixin, pluginBaseMixin],
+  props: {
+    stageData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data() {
+    return {
+      winHeight: 300,
+      logs: [],
+      steps: [],
+      status: '',
+    };
+  },
+  computed: {
+    releaseId() {
+      return this.$route.query.release_id;
+    },
+  },
+  watch: {
+    stageData: {
+      handler(newStageData) {
+        this.logs = newStageData.detail.logs;
+        this.steps = this.modifyDeployStepsData(newStageData.detail.steps);
+        this.status = newStageData.status;
+      },
+      deep: true,
+      immediate: true,
+    },
+  },
+  methods: {
+    getDisplayTime(payload) {
+      let theTime = payload;
+      if (theTime < 1) {
+        return `< 1${this.$t('秒')}`;
+      }
+      let middle = 0;
+      let hour = 0;
 
-                if (theTime > 60) {
-                    middle = parseInt(theTime / 60, 10);
-                    theTime = parseInt(theTime % 60, 10);
-                    if (middle > 60) {
-                        hour = parseInt(middle / 60, 10);
-                        middle = parseInt(middle % 60, 10);
-                    }
-                }
+      if (theTime > 60) {
+        middle = parseInt(theTime / 60, 10);
+        theTime = parseInt(theTime % 60, 10);
+        if (middle > 60) {
+          hour = parseInt(middle / 60, 10);
+          middle = parseInt(middle % 60, 10);
+        }
+      }
 
-                let result = '';
+      let result = '';
 
-                if (theTime > 0) {
-                    result = `${theTime}${this.$t('秒')}`;
-                }
-                if (middle > 0) {
-                    result = `${middle}${this.$t('分')}${result}`;
-                }
-                if (hour > 0) {
-                    result = `${hour}${this.$t('时')}${result}`;
-                }
+      if (theTime > 0) {
+        result = `${theTime}${this.$t('秒')}`;
+      }
+      if (middle > 0) {
+        result = `${middle}${this.$t('分')}${result}`;
+      }
+      if (hour > 0) {
+        result = `${hour}${this.$t('时')}${result}`;
+      }
 
-                return result;
-            },
+      return result;
+    },
 
-            /**
+    /**
              * 计算部署进程间的所花时间
              */
-             computedDeployTime (startTime, endTime) {
-                const start = new Date(startTime).getTime();
-                const end = new Date(endTime).getTime();
-                const interval = (end - start) / 1000;
+    computedDeployTime(startTime, endTime) {
+      const start = new Date(startTime).getTime();
+      const end = new Date(endTime).getTime();
+      const interval = (end - start) / 1000;
 
-                if (!interval) {
-                    return `< 1${this.$t('秒')}`;
-                }
+      if (!interval) {
+        return `< 1${this.$t('秒')}`;
+      }
 
-                return this.getDisplayTime(interval);
-            },
+      return this.getDisplayTime(interval);
+    },
 
-            modifyDeployStepsData (steps) {
-                return steps.map(item => {
-                    // da根据 steps[i].status 来决定当前部署的状态
-                    item.time = this.computedDeployTime(item.start_time, item.complete_time);
-                    if (item.status === 'successful' || item.status === 'failed') {
-                        item.tag = `
+    modifyDeployStepsData(steps) {
+      return steps.map((item) => {
+        // 根据 steps[i].status 来决定当前部署的状态
+        item.time = this.computedDeployTime(item.start_time, item.complete_time);
+        if (item.status === 'successful' || item.status === 'failed') {
+          item.tag = `
                         <div style="width: 185px" class="flex-row justify-content-between">
                             <div>${item.name}</div>
                             <div style="color: #C4C6CC" class="time-cls">${item.time}</div>
                         </div>
                         `;
-                    } else {
-                        item.tag = `
+        } else {
+          item.tag = `
                         <div style="width: 185px" class="flex-row justify-content-between">
                             <div>${item.name}</div>
                         </div>
                         `;
-                    }
-                    switch (item.status) {
-                        case 'successful':
-                            item.type = 'success';
-                            break;
-                        case 'failed':
-                            item.type = 'danger';
-                            break;
-                        case 'pending':
-                            item.type = 'primary';
-                            item.icon = <round-loading size="small" ext-cls="deploy-round-loading" />;
-                            break;
-                    }
-                    return item;
-                });
-            },
-            clickRerun (status) {
-                if (status !== 'pending') {
-                    this.$emit('rerunStage');
-                }
-            }
         }
-    };
+        switch (item.status) {
+          case 'successful':
+            item.type = 'success';
+            break;
+          case 'failed':
+            item.type = 'danger';
+            break;
+          case 'pending':
+            item.type = 'primary';
+            item.icon = <round-loading size="small" ext-cls="deploy-round-loading" />;
+            break;
+        }
+        return item;
+      });
+    },
+    clickRerun(status) {
+      if (status !== 'pending') {
+        this.$emit('rerunStage');
+      }
+    },
+  },
+};
 </script>

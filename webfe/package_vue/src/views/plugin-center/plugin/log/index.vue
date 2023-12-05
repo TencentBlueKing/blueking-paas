958771a3c2cee8530d6d778feae8c5192b7cc21b
leafage-collb <3303710797@qq.com>
2023-12-05 16:03:35 +0800

    fix: 标准输出、访问日志 TAB 根据 FeatureFlag 控制是否展示

--- webfe/package_vue/src/views/plugin-center/plugin/log/index.vue
+++ webfe/package_vue/src/views/plugin-center/plugin/log/index.vue
@@ -29,12 +29,14 @@
           <bk-tab-panel
             name="stream"
             :label="$t('标准输出日志')"
+            v-if="pluginFeatureFlags.STDOUT_LOG"
           >
             <standart-log v-if="tabActive === 'stream'" />
           </bk-tab-panel>
           <bk-tab-panel
             name="access"
             :label="$t('访问日志')"
+            v-if="pluginFeatureFlags.ACCESS_LOG"
           >
             <access-log
               v-if="tabActive === 'access'"
@@ -48,58 +50,58 @@
 </template>
 
 <script>
-    import pluginBaseMixin from '@/mixins/plugin-base-mixin';
-    import standartLog from './standart-log.vue';
-    import accessLog from './access-log.vue';
-    import customLog from './custom-log.vue';
-    import paasPluginTitle from '@/components/pass-plugin-title';
+import pluginBaseMixin from '@/mixins/plugin-base-mixin';
+import standartLog from './standart-log.vue';
+import accessLog from './access-log.vue';
+import customLog from './custom-log.vue';
+import paasPluginTitle from '@/components/pass-plugin-title';
 
-    export default {
-        components: {
-            standartLog,
-            accessLog,
-            customLog,
-            paasPluginTitle
-        },
-        mixins: [pluginBaseMixin],
-        data () {
-            return {
-                name: 'log-component',
-                tabActive: 'structured',
-                tabChangeIndex: 0,
-                isLoading: false
-            };
-        },
-        computed: {
-            pluginFeatureFlags () {
-                return this.$store.state.plugin.pluginFeatureFlags;
-            }
-        },
-        mounted () {
-            this.isLoading = true;
-            if (this.$route.query.tab) {
-                const isExistTab = ['stream', 'access'].includes(this.$route.query.tab);
-                this.tabActive = isExistTab ? this.$route.query.tab : 'stream';
-            }
-            setTimeout(() => {
-                this.isLoading = false;
-            }, 2000);
-        },
-        methods: {
-            handleTabChange (payload) {
-                this.$router.push({
-                    name: 'pluginLog',
-                    params: {
-                        id: this.$route.params.id,
-                        moduleId: this.$route.params.pluginTypeId
-                    },
-                    query: {
-                        tab: payload
-                    }
-                });
-            }
-        }
+export default {
+  components: {
+    standartLog,
+    accessLog,
+    customLog,
+    paasPluginTitle,
+  },
+  mixins: [pluginBaseMixin],
+  data() {
+    return {
+      name: 'log-component',
+      tabActive: 'structured',
+      tabChangeIndex: 0,
+      isLoading: false,
     };
+  },
+  computed: {
+    pluginFeatureFlags() {
+      return this.$store.state.plugin.pluginFeatureFlags;
+    },
+  },
+  mounted() {
+    this.isLoading = true;
+    if (this.$route.query.tab) {
+      const isExistTab = ['stream', 'access'].includes(this.$route.query.tab);
+      this.tabActive = isExistTab ? this.$route.query.tab : 'stream';
+    }
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  },
+  methods: {
+    handleTabChange(payload) {
+      this.$router.push({
+        name: 'pluginLog',
+        params: {
+          id: this.$route.params.id,
+          moduleId: this.$route.params.pluginTypeId,
+        },
+        query: {
+          tab: payload,
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>

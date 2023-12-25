54fb662ba9c1fc90590bc1eba0bd9ad2de4daab4
jamesgetx <gejun.coolfriend@gmail.com>
2023-12-25 11:59:27 +0800

    fix: add bash /runner/init prefix to scripts for s-mart app
    N

--- apiserver/paasng/paasng/platform/engine/deploy/image_release.py
+++ apiserver/paasng/paasng/platform/engine/deploy/image_release.py
@@ -18,6 +18,7 @@ to the current version of the project delivered to anyone in the future.
 """
 import logging
 
+import cattr
 from celery import shared_task
 from django.utils.translation import gettext as _
 
@@ -28,10 +29,13 @@ from paas_wl.bk_app.processes.models import ProcessTmpl
 from paas_wl.workloads.images.models import AppImageCredential
 from paasng.accessories.servicehub.manager import mixed_service_mgr
 from paasng.platform.applications.constants import ApplicationType
+from paasng.platform.bkapp_model.constants import DEFAULT_SLUG_RUNNER_ENTRYPOINT
 from paasng.platform.bkapp_model.manager import sync_to_bkapp_model
 from paasng.platform.bkapp_model.models import ModuleProcessSpec
+from paasng.platform.declarative.deployment.resources import Scripts
 from paasng.platform.declarative.exceptions import ControllerError, DescriptionValidationError
 from paasng.platform.declarative.handlers import AppDescriptionHandler
+from paasng.platform.declarative.models import DeploymentDescription
 from paasng.platform.engine.configurations.image import ImageCredentialManager, RuntimeImageInfo, get_credential_refs
 from paasng.platform.engine.constants import JobStatus
 from paasng.platform.engine.deploy.release import start_release_step
@@ -88,7 +92,7 @@ class ImageReleaseMgr(DeployStep):
                 # 2. 仅托管镜像的应用(包含云原生应用和旧镜像应用)
                 if is_smart_app:
                     # S-Mart 应用使用 S-Mart 包的元信息记录启动进程
-                    self.try_handle_app_description()
+                    self.handle_smart_app_description()
                     processes = list(get_processes(deployment=self.deployment).values())
                     # 保存应用描述文件记录的信息到 DB - Processes/Hooks
                     sync_to_bkapp_model(module=module, processes=processes, hooks=self.deployment.get_deploy_hooks())
@@ -182,8 +186,12 @@ class ImageReleaseMgr(DeployStep):
 
         handler.handle_deployment(self.deployment)
 
-    def try_handle_app_description(self):
-        """A tiny wrapper for _handle_app_description, will ignore all exception raise from _handle_app_description"""
+    def handle_smart_app_description(self):
+        """A tiny wrapper for _handle_app_description, will ignore all exception raise from _handle_app_description.
+        And will do something for s-mart app
+
+        - complete scripts command
+        """
         try:
             self._handle_app_description()
         except FileNotFoundError:
@@ -197,3 +205,12 @@ class ImageReleaseMgr(DeployStep):
         except Exception:
             self.stream.write_message(Style.Error(_("处理应用描述文件时出现异常, 请检查应用描述文件")))
             logger.exception("Exception while processing app description file, skip.")
+
+        # complete scripts command
+        description = DeploymentDescription.objects.get(deployment=self.deployment)
+
+        scripts = cattr.structure(description.scripts, Scripts)
+        scripts.pre_release_hook = f"{(' ').join(DEFAULT_SLUG_RUNNER_ENTRYPOINT)} {scripts.pre_release_hook}"
+        description.scripts = cattr.unstructure(scripts)
+
+        description.save(update_fields=["scripts"])

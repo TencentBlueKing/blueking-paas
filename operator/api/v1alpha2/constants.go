10b06a96868f9bb644498e9c63488f18e8ba3e86
shabbywu <shabbywu@tencent.com>
2024-01-22 03:23:05 +0000

    feature: operator 支持挂载应用日志到宿主机 (#1074)

--- operator/api/v1alpha2/constants.go
+++ operator/api/v1alpha2/constants.go
@@ -69,6 +69,17 @@ const (
 	LegacyProcResAnnoKey = "bkapp.paas.bk.tencent.com/legacy-proc-res-config"
 )
 
+// 日志采集相关信息
+const (
+	// LogCollectorTypeAnnoKey 注解中存储日志采集类型相关的键名
+	LogCollectorTypeAnnoKey = "bkapp.paas.bk.tencent.com/log-collector-type"
+
+	// BuiltinELKCollector 平台内置的 ELK 采集方案(需要挂载日志目录到宿主机指定位置)
+	BuiltinELKCollector = "ELK"
+	// BkLogCollector 日志平台采集器
+	BkLogCollector = "BK_LOG"
+)
+
 // 平台相关信息
 const (
 	// HookTypeKey 注解中存储钩子类型的键名

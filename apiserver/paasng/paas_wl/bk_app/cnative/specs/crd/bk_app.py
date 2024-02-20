63647bdec52ba0a27894e8d80b468e2381bc81b3
shabbywu <shabbywu@tencent.com>
2024-02-20 12:23:21 +0800

    refactor: 使用 deploy-id 避免误判 bkapp 的部署结果

--- apiserver/paasng/paas_wl/bk_app/cnative/specs/crd/bk_app.py
+++ apiserver/paasng/paas_wl/bk_app/cnative/specs/crd/bk_app.py
@@ -254,6 +254,7 @@ class BkAppStatus(BaseModel):
     observedGeneration: int = Field(default=0)
     conditions: List[MetaV1Condition] = Field(default_factory=list)
     lastUpdate: Optional[datetime.datetime]
+    deployId: str = ""
 
 
 class BkAppResource(BaseModel):
@@ -280,7 +281,4 @@ class BkAppResource(BaseModel):
         # entries such as `"hooks": null` is not processable in Kubernetes 1.18.
         result = self.dict(exclude_none=True, exclude={"status"})
         result["metadata"].pop("generation", None)
-        # MUST set status.phase to pending, otherwise the deployment detection process
-        # MAYBE misjudge that the deployment is successful immediately when operator is too slow to reconcile the event.
-        result["status"] = {"phase": MResPhaseType.AppPending}
         return result

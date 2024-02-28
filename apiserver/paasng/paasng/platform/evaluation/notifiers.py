084707ab5af18fe00a2c0270025dc130120d2c22
schnee <suzh9@mail2.sysu.edu.cn>
2024-02-28 19:52:51 +0800

    minor: update comments

--- apiserver/paasng/paasng/platform/evaluation/notifiers.py
+++ apiserver/paasng/paasng/platform/evaluation/notifiers.py
@@ -5,11 +5,14 @@ TencentBlueKing is pleased to support the open source community by making
 Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
 Licensed under the MIT License (the "License"); you may not use this file except
 in compliance with the License. You may obtain a copy of the License at
+
     http://opensource.org/licenses/MIT
+
 Unless required by applicable law or agreed to in writing, software distributed under
 the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
 either express or implied. See the License for the specific language governing permissions and
 limitations under the License.
+
 We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
@@ -28,6 +31,7 @@ class AppOperationReportNotifier:
         self.issue_type = issue_type
 
     def send(self, receiver_type: EmailReceiverType, receivers: List[str]):
+        # 该版本暂时不支持发送邮件，有需要可以在 notifiers_ext 中实现同名类
         logger.warning("send operation report is unsupported")
 
 

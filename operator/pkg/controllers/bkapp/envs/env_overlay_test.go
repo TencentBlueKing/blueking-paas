028fa30c65a8fdf5e794913e7d7486224064370c
dijiachen <dijiachen@tencent.com>
2024-03-27 09:45:12 +0800

    feat: cpu 和 memory requests 支持可配置

--- operator/pkg/controllers/bkapp/envs/env_overlay_test.go
+++ operator/pkg/controllers/bkapp/envs/env_overlay_test.go
@@ -26,7 +26,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 
 	. "github.com/onsi/ginkgo/v2"

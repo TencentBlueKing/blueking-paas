a368e7129735d8a965b95005623f1c84cc6e2ed7
jiayuan929 <252461528@qq.com>
2024-04-10 11:48:30 +0800

    feat: 查询我的应用时也需要返回总的应用数 (#1235)

--- apiserver/paasng/paasng/platform/applications/views.py
+++ apiserver/paasng/paasng/platform/applications/views.py
@@ -143,7 +143,6 @@ class ApplicationViewSet(viewsets.ViewSet):
 
         # Get applications by given params
         applications = UserApplicationFilter(request.user).filter(
-            exclude_collaborated=params.get("exclude_collaborated"),
             include_inactive=params["include_inactive"],
             languages=params.get("language"),
             regions=params.get("region"),
@@ -152,6 +151,11 @@ class ApplicationViewSet(viewsets.ViewSet):
             type_=params.get("type"),
             order_by=[params.get("order_by")],
         )
+        # 查询我创建的应用时，也需要返回总的应用数量给前端
+        all_app_count = applications.count()
+        # 仅查询我创建的应用
+        if params.get("exclude_collaborated") is True:
+            applications = applications.filter(owner=request.user.pk)
 
         # 插件开发者中心正式上线前需要根据配置来决定应用列表中是否展示插件应用
         if not settings.DISPLAY_BK_PLUGIN_APPS:
@@ -198,6 +202,7 @@ class ApplicationViewSet(viewsets.ViewSet):
                 "engineless_app_count": engineless_app_count,
                 "cloud_native_app_count": cloud_native_app_count,
                 "my_app_count": my_app_count,
+                "all_app_count": all_app_count,
             },
         )
 

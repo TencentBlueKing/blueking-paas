25b4f9edf0968ddf890c3f5b2fec07f7ad2e5a62
wmh <3303710797@qq.com>
2024-05-21 16:12:29 +0800

    chore: cherry-pick
    N

--- webfe/package_vue/src/views/dev-center/app/engine/cloud-deploy-manage/comps/deploy-dialog.vue
+++ webfe/package_vue/src/views/dev-center/app/engine/cloud-deploy-manage/comps/deploy-dialog.vue
@@ -205,7 +205,7 @@
               v-for="(branch, index) in branchList"
               :key="index"
               class="option-group"
-              :name="branch.name"
+              :name="branch.displayName"
             >
               <bk-button
                 ext-cls="paas-branch-btn"
@@ -797,6 +797,7 @@ export default {
             branchesList.push({
               id: branch.type,
               name: branch.type,
+              displayName: branch.display_type,
               children: [obj],
             });
           } else {

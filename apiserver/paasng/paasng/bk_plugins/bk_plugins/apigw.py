f9d40d50584a31ec3d3864a9e0f01756c0d6c183
jiayuan929 <252461528@qq.com>
2024-06-13 17:55:24 +0800

    chore: 添加注释

--- apiserver/paasng/paasng/bk_plugins/bk_plugins/apigw.py
+++ apiserver/paasng/paasng/bk_plugins/bk_plugins/apigw.py
@@ -147,6 +147,7 @@ class PluginDefaultAPIGateway:
         profile = self.plugin_app.bk_plugin_profile
         # When name is absent in plugin profile, generate a new name
         # WARN: The default value for gateway name should not exceeds 20 characters long.
+        # WARN: BlueKing official product gateways use the prefix bk- and need to be distinguished from them.
         self.gw_name = profile.api_gw_name or f"bp-{self.plugin_app.code}"
 
     def sync(self) -> int:

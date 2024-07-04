f2e6e1f6bbd4149a37a601118e941a0ef66048d9
tiga <68499559+SheepSheepChen@users.noreply.github.com>
2024-07-04 15:46:19 +0800

    refactor: SaaS 告警通知模版模版化 (#1423)

--- apiserver/paasng/paasng/misc/monitoring/monitor/alert_rules/ascode/client.py
+++ apiserver/paasng/paasng/misc/monitoring/monitor/alert_rules/ascode/client.py
@@ -85,12 +85,13 @@ class AsCodeClient:
             if code_snippet in alert_code:
                 ctx[f"{code_snippet}_metric_name_prefix"] = config.get("metric_name_prefix", "")
 
-    def _update_docs_url(self, ctx: dict, alert_code: str):
-        """根据告警代码添加需要的文档信息"""
+    def _get_docs_url(self, alert_code: str):
+        """根据告警代码获取需要的文档信息"""
         tag = get_dynamic_tag(f"saas_monitor:{alert_code}")
         links = DocumentaryLinkAdvisor().search_by_tags([tag], limit=1)
         if links:
-            ctx["doc_url"] = f"处理建议: {links[0].location}"
+            return f"。处理建议: {links[0].location}"
+        return ""
 
     def _render_rule_configs(self, rule_configs: List[RuleConfig]) -> Dict:
         """按照 MonitorAsCode 规则, 渲染出如下示例目录结构:
@@ -104,13 +105,16 @@ class AsCodeClient:
         j2_env = jinja2.Environment(loader=loader, trim_blocks=True)
 
         configs = {}
+        # 告警通知内容模版
+        notice_template = j2_env.get_template("notice_template.yaml.j2")
         for conf in rule_configs:
             ctx = conf.to_dict()
             ctx["notice_group_name"] = self.default_notice_group_name
             # 涉及到增强服务的告警策略, 指标是通过 bkmonitor 配置的采集器采集, 需要添加指标前缀
             self._update_metric_name_prefixes(ctx, conf.alert_code)
-            # 为通知添加操作文档链接，若未在'管理后台--智能顾问--文档管理'配置文档则不添加
-            self._update_docs_url(ctx, conf.alert_code)
+            # 为告警通知添加内容，若在'管理后台--智能顾问--文档管理'配置文档则额外添加处理文档链接
+            notice_template_content = notice_template.render(doc_url=self._get_docs_url(conf.alert_code))
+            ctx["notice_template"] = notice_template_content
             configs[f"rule/{conf.alert_rule_name}.yaml"] = j2_env.get_template(f"{conf.alert_code}.yaml.j2").render(
                 **ctx
             )

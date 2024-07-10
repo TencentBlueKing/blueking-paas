f1f1f40be2354c5d2b4166fe5ac9098eae66ef40
jamesgetx <gejun.coolfriend@gmail.com>
2024-07-10 11:42:56 +0800

    feat: support set custom domain ingress class separately
    N

--- apiserver/paasng/paasng/settings/workloads.py
+++ apiserver/paasng/paasng/settings/workloads.py
@@ -155,6 +155,8 @@ DEFAULT_POD_LOGS_LINE = 512
 
 # 不指定则使用默认，可以指定为 bk-ingress-nginx
 APP_INGRESS_CLASS = settings.get("APP_INGRESS_CLASS")
+# 独立域名的 ingress 使用的 ingress class
+CUSTOM_DOMAIN_INGRESS_CLASS = settings.get("CUSTOM_DOMAIN_INGRESS_CLASS")
 
 # 控制 ingress 资源路径是否严格匹配末尾斜杆, 如某个 ingress 路径设置成 "/foo/", 开启严格匹配将无法通过 "/foo" 访问应用
 # 如果希望通过 "/foo" 也能访问, 则需要设置 APP_INGRESS_EXT_V1BETA1_PATH_TRAILING_SLASH、APP_INGRESS_V1_PATH_TRAILING_SLASH 为 False

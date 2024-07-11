87a18a839b8e7f179adbc6509bb22badc8417f69
jiayuan929 <252461528@qq.com>
2024-07-11 11:31:20 +0800

    fix: untranslated internationalization fields issue (#1460)

--- apiserver/paasng/paas_wl/workloads/networking/entrance/serializers.py
+++ apiserver/paasng/paas_wl/workloads/networking/entrance/serializers.py
@@ -18,7 +18,7 @@
 import re
 from typing import Dict, Optional, Type
 
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import UniqueTogetherValidator

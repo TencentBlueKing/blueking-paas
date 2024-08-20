e5034bf2063e071f624e915e9a34b2261deb568d
dijiachen <dijiachen@tencent.com>
2024-08-20 19:51:38 +0800

    fix: update_app_resource 函数添加 metadata 校验

--- apiserver/paasng/paas_wl/bk_app/cnative/specs/models/app_resource.py
+++ apiserver/paasng/paas_wl/bk_app/cnative/specs/models/app_resource.py
@@ -245,7 +245,7 @@ def update_app_resource(app: Application, module: Module, payload: Dict):
         # force replace metadata.name with app_code to avoid user modify
         payload["metadata"]["name"] = generate_bkapp_name(module)
     except KeyError as e:
-        raise ValidationError(f"Missing field in payload: {e}")
+        raise ValidationError(f"Missing field: {e}")
 
     try:
         obj = BkAppResource(**payload)

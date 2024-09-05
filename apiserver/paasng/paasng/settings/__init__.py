2fc34eae4ad13823b11840b43b9379702f205ec0
林晓武[finelin] <finelin@canway.net>
2024-09-05 14:46:45 +0800

    fix: 带端口的镜像地址解析问题 #1576

--- apiserver/paasng/paasng/settings/__init__.py
+++ apiserver/paasng/paasng/settings/__init__.py
@@ -1254,9 +1254,9 @@ SMART_DOCKER_REGISTRY_PASSWORD = settings.get("SMART_DOCKER_PASSWORD", "blueking
 # S-Mart 基础镜像信息
 _SMART_TAG_SUFFIX = "smart"
 SMART_IMAGE_NAME = f"{SMART_DOCKER_REGISTRY_NAMESPACE}/slug-pilot"
-SMART_IMAGE_TAG = f'{settings.get("APP_IMAGE", "").partition(":")[-1]}-{_SMART_TAG_SUFFIX}'
+SMART_IMAGE_TAG = f'{settings.get("APP_IMAGE", "").split(":")[-1]}-{_SMART_TAG_SUFFIX}'
 SMART_CNB_IMAGE_NAME = f"{SMART_DOCKER_REGISTRY_NAMESPACE}/run-heroku-bionic"
-SMART_CNB_IMAGE_TAG = f'{settings.get("HEROKU_RUNNER_IMAGE", "").partition(":")[-1]}-{_SMART_TAG_SUFFIX}'
+SMART_CNB_IMAGE_TAG = f'{settings.get("HEROKU_RUNNER_IMAGE", "").split(":")[-1]}-{_SMART_TAG_SUFFIX}'
 
 # slugbuilder build 的超时时间, 单位秒
 BUILD_PROCESS_TIMEOUT = int(settings.get("BUILD_PROCESS_TIMEOUT", 60 * 15))

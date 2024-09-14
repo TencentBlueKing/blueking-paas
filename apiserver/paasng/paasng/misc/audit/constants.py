9a45eab59b06145cbda84ee922db5e5922680a0a
lyzqs <34978855+lyzqs@users.noreply.github.com>
2024-09-14 16:15:52 +0800

    refactor: admin42 buildpack 管理功能重构 (#1586)
    Co-authored-by: schnee <suzh9@mail2.sysu.edu.cn>

--- apiserver/paasng/paasng/misc/audit/constants.py
+++ apiserver/paasng/paasng/misc/audit/constants.py
@@ -87,6 +87,7 @@ class OperationTarget(str, StructuredEnum):
     FEATURE_FLAG = EnumField("feature_flag", label=_("特性标记"))
     EGRESS_SPEC = EnumField("egress_spec", label=_("Egress 配置"))
     TEMPLATE = EnumField("template", label=_("模板"))
+    BUILDPACK = EnumField("buildpack", label="Buildpack")
 
 
 class OperationEnum(str, StructuredEnum):

42e39cef060fe5bd9441502be76ac301b316d483
leafage-collb <3303710797@qq.com>
2024-09-18 15:19:23 +0800

    fix: 应用迁移：有正在迁移的应用导致其他应用的迁移按钮不能点击时需要给出 tips 说明 # Reviewed, transaction id: 18668

--- webfe/package_vue/src/language/lang/en.js
+++ webfe/package_vue/src/language/lang/en.js
@@ -2810,4 +2810,5 @@ export default {
   测试号: 'Test number',
   已终止: 'Terminated',
   '主版本号、次版本号、修正版本号': 'Major version number, minor version number, patch version number',
+  '当前有其他应用（{id}）正在迁移，需待迁移完成后才能开启新应用的迁移。': 'Another application ({id}) is currently being migrated. Please wait until the migration is complete before starting a new application migration.',
 };

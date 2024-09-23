cef69a8cb96efb2469172fa34fa7612ce429bc62
jiayuan929 <252461528@qq.com>
2024-09-23 12:46:55 +0800

    fix: webfe 中 BK_PAAS_DOCS_VER 定义

--- webfe/package_vue/static/runtime/runtime.ee.js
+++ webfe/package_vue/static/runtime/runtime.ee.js
@@ -29,6 +29,7 @@ var BK_ANALYSIS_JS = process.env.BK_ANALYSIS_JS || '' //上报js 内部版需要
 var BK_PAAS_VERSION = process.env.BK_PAAS_VERSION || ''
 var BK_MAX_PACKAGE_SIZE = process.env.BK_MAX_PACKAGE_SIZE || ''
 var BK_SHARED_RES_URL = process.env.BK_SHARED_RES_URL || ''
+var BK_PAAS_DOCS_VER = process.env.BK_PAAS_DOCS_VER || ''
 
 var BACKEND_URL = `${SITE_URL}/backend` // 后端接口前缀
 var PAAS_DOCS_URL_PREFIX = `${BK_DOCS_URL_PREFIX}/markdown/ZH/PaaS/${BK_PAAS_DOCS_VER}` // PaaS 产品文档前缀

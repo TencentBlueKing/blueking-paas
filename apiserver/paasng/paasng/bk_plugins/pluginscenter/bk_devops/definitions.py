6430349bfbe3b98ea4ac00652b30c147c6ef54c3
jiayuan929 <252461528@qq.com>
2024-09-24 19:18:13 +0800

    fix: codecc 基本信息 API 返回字段都可为空 (#1627)

--- apiserver/paasng/paasng/bk_plugins/pluginscenter/bk_devops/definitions.py
+++ apiserver/paasng/paasng/bk_plugins/pluginscenter/bk_devops/definitions.py
@@ -311,9 +311,9 @@ class CodeccPluginBasicInfo(BaseModel):
     :param description: 工具描述
     """
 
-    name: str
-    displayName: str
-    checkerNum: int
+    name: Optional[str] = None
+    displayName: Optional[str] = None
+    checkerNum: Optional[int] = None
     devLanguage: Optional[str] = None
     toolCnTypes: Optional[List[str]] = []
     langList: Optional[List[str]] = []

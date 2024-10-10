90e32298147a089bd7028fd0b267f01e25f37104
jiayuan929 <252461528@qq.com>
2024-10-10 11:08:32 +0800

    feat: 插件审批的标题中用插件名称而不是 ID

--- apiserver/paasng/paasng/bk_plugins/pluginscenter/itsm_adaptor/utils.py
+++ apiserver/paasng/paasng/bk_plugins/pluginscenter/itsm_adaptor/utils.py
@@ -58,7 +58,7 @@ def submit_create_approval_ticket(pd: PluginDefinition, plugin: PluginInstance, 
 
     # 组装提单数据,包含插件的基本信息
     basic_fields = _get_basic_fields(pd, plugin)
-    title_fields = [{"key": "title", "value": f"插件[{plugin.id}]创建审批"}]
+    title_fields = [{"key": "title", "value": f"插件[{plugin.name}]创建审批"}]
     fields = basic_fields + title_fields
 
     # 提交 itsm 申请单据
@@ -80,7 +80,7 @@ def submit_online_approval_ticket(pd: PluginDefinition, plugin: PluginInstance, 
     # 组装提单数据,包含插件的基本信息和版本信息
     basic_fields = _get_basic_fields(pd, plugin)
     advanced_fields = _get_advanced_fields(pd, plugin, version)
-    title_fields = [{"key": "title", "value": f"插件[{plugin.id}]上线审批"}]
+    title_fields = [{"key": "title", "value": f"插件[{plugin.name}]上线审批"}]
     fields = basic_fields + advanced_fields + title_fields
 
     # 查询上线审批服务ID
@@ -131,7 +131,7 @@ def submit_canary_release_ticket(
 
     # 组装提单数据,包含插件的基本信息和灰度发布信息
     basic_fields = _get_basic_fields(pd, plugin)
-    title_fields = [{"key": "title", "value": f"插件[{plugin.id}]灰度发布审批"}]
+    title_fields = [{"key": "title", "value": f"插件[{plugin.name}]灰度发布审批"}]
     itsm_fields = basic_fields + title_fields + canary_fields
 
     service_name = release_strategy.get_itsm_service_name()
@@ -196,7 +196,7 @@ def submit_visible_range_ticket(
 
     # 组装提单数据,包含插件的基本信息、可见范围修改前的值，修改后的值
     basic_fields = _get_basic_fields(pd, plugin)
-    title_fields = [{"key": "title", "value": f"插件[{plugin.id}]可见范围修改审批"}]
+    title_fields = [{"key": "title", "value": f"插件[{plugin.name}]可见范围修改审批"}]
     fields = basic_fields + title_fields + visible_range_fields
 
     # 提交 itsm 申请单据

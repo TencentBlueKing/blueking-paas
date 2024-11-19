959b9ac2a721719b434237a09b40c6c568ae23bc
dijiachen <dijiachen@tencent.com>
2024-11-19 15:57:14 +0800

    feat: 云原生应用支持出口 ip 功能

--- operator/api/v1alpha2/constants.go
+++ operator/api/v1alpha2/constants.go
@@ -75,7 +75,7 @@ const (
 	LegacyProcResAnnoKey = "bkapp.paas.bk.tencent.com/legacy-proc-res-config"
 
 	// EgressClusterStateNameAnnoKey 注解中存储 Egress 相关配置的键名
-	// 在集群中，用于出口 ip 的节点打上了 {EgressClusterStateNameAnnoKey: 1} 的标签
+	// 在集群中，用于出口 ip 的节点打上了 {EgressClusterStateNameAnnoKey:"1"} 的标签
 	// 用于保证开启了出口 ip 功能的应用只会调度到相应的节点
 	EgressClusterStateNameAnnoKey = "bkapp.paas.bk.tencent.com/egress-cluster-state-name"
 )

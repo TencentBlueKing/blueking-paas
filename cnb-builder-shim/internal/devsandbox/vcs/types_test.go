d25cbeccbe9445158041bf1943c4a4bc00873213
schnee <suzh9@mail2.sysu.edu.cn>
2024-12-13 14:47:00 +0800

    fix: remove / in root diff tree name

--- cnb-builder-shim/internal/devsandbox/vcs/types_test.go
+++ cnb-builder-shim/internal/devsandbox/vcs/types_test.go
@@ -39,7 +39,7 @@ var _ = Describe("Test Types", func() {
 			}
 
 			excepted := &DirTree{
-				Name: "/",
+				Name: "",
 				Dirs: []*DirTree{
 					{
 						Name: "api", Files: Files{
@@ -93,7 +93,7 @@ var _ = Describe("Test Types", func() {
 			}
 
 			excepted := &DirTree{
-				Name: "/",
+				Name: "",
 				Dirs: []*DirTree{
 					{
 						Name: "api",

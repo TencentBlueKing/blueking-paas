4d7719f4e22f99b90f5c979e45181cdb59e8539a
songzxc789 <sxdzec@qq.com>
2024-12-24 10:10:28 +0800

    fix: tranform bkmonitor in app_desc.yaml spec_version 2 (#1815)

--- apiserver/paasng/paasng/misc/tools/app_desc.py
+++ apiserver/paasng/paasng/misc/tools/app_desc.py
@@ -17,21 +17,19 @@
 
 import re
 from collections import OrderedDict
-from typing import Any
+from typing import Any, Dict, List, Union
 
 from django.conf import settings
 
 from paasng.utils.camel_converter import snake_to_camel
 
 
-def transform_app_desc_spec2_to_spec3(spec2):
+def transform_app_desc_spec2_to_spec3(spec2: Dict[str, Any]) -> OrderedDict[str, Any]:
     """
     Converts an application description file from spec_version 2 to specVersion 3 format.
 
     :param spec2: Application description in spec_version 2 format.
-    :type spec2: dict
     :return: Transformed application description in specVersion 3 format.
-    :rtype: dict
     """
     spec3: OrderedDict[str, Any] = OrderedDict()
 
@@ -45,7 +43,7 @@ def transform_app_desc_spec2_to_spec3(spec2):
     if "modules" in spec2:
         spec3["modules"] = transform_modules_section(spec2["modules"])
     elif "module" in spec2:
-        # 只有一个 module 时, module 字典在第一层
+        # When there is only one module, 'module' is at the first level as a dictionary
         module = spec2["module"]
         module["name"] = "default"
         spec3["module"] = transform_module(module)
@@ -53,14 +51,12 @@ def transform_app_desc_spec2_to_spec3(spec2):
     return spec3
 
 
-def transform_app_section(app):
+def transform_app_section(app: Dict[str, Any]) -> Dict[str, Any]:
     """
     Transforms the 'app' section.
 
     :param app: The 'app' section from the spec_version 2 format.
-    :type app: dict
     :return: Transformed 'app' section in specVersion 3 format.
-    :rtype: dict
     """
     return {
         snake_to_camel(key): transform_display_options(value) if key == "market" and isinstance(value, dict) else value
@@ -68,14 +64,12 @@ def transform_app_section(app):
     }
 
 
-def transform_display_options(display_options):
+def transform_display_options(display_options: Dict[str, Any]) -> Dict[str, Any]:
     """
     Transforms the 'displayOptions' section within 'market' in 'app'.
 
     :param display_options: The 'display_options' dictionary from spec_version 2 format.
-    :type display_options: dict
     :return: Transformed 'displayOptions' dictionary in specVersion 3 format.
-    :rtype: dict
     """
     return {
         snake_to_camel(mk): {snake_to_camel(dk): dv for dk, dv in mv.items()} if isinstance(mv, dict) else mv
@@ -83,55 +77,50 @@ def transform_display_options(display_options):
     }
 
 
-def transform_modules_section(modules):
+def transform_modules_section(modules: Dict[str, Dict[str, Any]]) -> List[OrderedDict[str, Any]]:
     """
     Transforms the 'modules' section.
 
     :param modules: The 'modules' section from the spec_version 2 format.
-    :type modules: dict
     :return: Transformed 'modules' section in specVersion 3 format.
-    :rtype: list
     """
     return [transform_module({**module, "name": module_name}) for module_name, module in modules.items()]
 
 
-def transform_module(module):
+def transform_module(module: Dict[str, Any]) -> OrderedDict[str, Any]:
     """
     Transforms a single module.
 
     :param module: The module from the spec_version 2 format.
-    :type module: dict
     :return: Transformed module in specVersion 3 format.
-    :rtype: OrderedDict
     """
-    new_module = OrderedDict()
+    new_module: OrderedDict[str, Any] = OrderedDict()
 
     new_module["name"] = module["name"]
-    # 处理 module 内容
+
+    # Process the module content
     for key, value in module.items():
         if key in ["name", "package_plans"]:
             continue
-        if key in ["services", "env_variables", "processes", "scripts", "svc_discovery"]:
-            # 收集这些到 module.spec
+        if key in ["services", "env_variables", "processes", "scripts", "svc_discovery", "bkmonitor"]:
+            # Collect these into module.spec
             if "spec" not in new_module:
                 new_module["spec"] = OrderedDict()
             transform_module_spec(new_module["spec"], key, value)
         else:
-            # 其他, 即 is_default, source_dir 或 language
+            # Others, such as is_default, source_dir, or language
             new_module[snake_to_camel(key)] = value
+
     return new_module
 
 
-def transform_module_spec(spec, key, value):
+def transform_module_spec(spec: OrderedDict[str, Any], key: str, value: Any) -> None:
     """
-    Transforms module fields like 'services', 'env_variables', 'processes', 'scripts', and 'svc_discovery' into 'spec'.
+    Transforms module fields like 'services', 'env_variables', 'processes', 'scripts', 'svc_discovery' and 'bkmonitor' into 'spec'.
 
     :param spec: The spec dictionary where the transformed data will be added.
-    :type spec: OrderedDict
     :param key: The name of the field (e.g., 'services', 'env_variables').
-    :type key: str
     :param value: The content of the field to be transformed.
-    :type value: dict
     """
     if key == "services":
         spec["addons"] = transform_services(value)
@@ -140,31 +129,31 @@ def transform_module_spec(spec, key, value):
     elif key == "processes":
         spec["processes"] = transform_processes(value)
     elif key == "scripts":
-        spec["hooks"] = {"preRelease": {"procCommand": value.get("pre_release_hook")}}
+        spec["hooks"] = {"preRelease": {"procCommand": value["pre_release_hook"]}}
     elif key == "svc_discovery" and "bk_saas" in value:
         spec["svcDiscovery"] = {"bkSaaS": transform_bk_saas(value["bk_saas"])}
+    elif key == "bkmonitor":
+        metrics = tranform_bkmonitor(spec["processes"], value["port"])
+        if metrics:
+            spec["observability"] = {"monitoring": {"metrics": metrics}}
 
 
-def transform_services(services):
+def transform_services(services: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
     """
     Transforms the 'services' field of module.
 
     :param services: A list of services from spec_version 2 format.
-    :type services: list
     :return: Transformed list of services in specVersion 3 format.
-    :rtype: list
     """
     return [{snake_to_camel(k): v for k, v in service.items()} for service in services]
 
 
-def transform_env_variables(env_vars):
+def transform_env_variables(env_vars: List[Dict[str, Any]]) -> List[OrderedDict[str, Any]]:
     """
     Transforms the 'env_variables' field of module.
 
     :param env_vars: A list of environment variables from spec_version 2 format.
-    :type env_vars: list
     :return: Transformed list of environment variables in specVersion 3 format.
-    :rtype: list
     """
     return [
         OrderedDict(("name" if k == "key" else snake_to_camel(k), v) for k, v in env_var.items())
@@ -172,30 +161,25 @@ def transform_env_variables(env_vars):
     ]
 
 
-def transform_processes(processes):
+def transform_processes(processes: Dict[str, Dict[str, Any]]) -> List[OrderedDict[str, Any]]:
     """
     Transforms the 'processes' field of module.
 
     :param processes: The 'processes' section from the spec_version 2 format.
-    :type processes: dict
     :return: Transformed list of processes in specVersion 3 format.
-    :rtype: list
     """
     return [transform_process(process_name, process_info) for process_name, process_info in processes.items()]
 
 
-def transform_process(process_name, process_info):
+def transform_process(process_name: str, process_info: Dict[str, Any]) -> OrderedDict[str, Any]:
     """
     Transforms a single process, adding associated service information.
 
-    :param process_name: The name of the process (e.g., 'web', 'wroker').
-    :type process_name: str
+    :param process_name: The name of the process (e.g., 'web', 'worker').
     :param process_info: The information about the process.
-    :type process_info: dict
     :return: Transformed process information in specVersion 3 format.
-    :rtype: OrderedDict
     """
-    transformed_process = OrderedDict([("name", process_name)])
+    transformed_process: OrderedDict[str, Any] = OrderedDict([("name", process_name)])
 
     for key, value in process_info.items():
         if key == "command":
@@ -212,14 +196,12 @@ def transform_process(process_name, process_info):
     return transformed_process
 
 
-def create_service(process_name):
+def create_service(process_name: str) -> Dict[str, Union[str, int]]:
     """
     Creates service information for the given process name.
 
     :param process_name: The name of the process.
-    :type process_name: str
     :return: Service information for the process.
-    :rtype: dict
     """
     if process_name == "web":
         service = {
@@ -240,14 +222,12 @@ def create_service(process_name):
     return service
 
 
-def transform_process_probes(probes):
+def transform_process_probes(probes: Dict[str, Dict[str, Any]]) -> OrderedDict[str, OrderedDict[str, Any]]:
     """
     Transforms the process probes in process.
 
     :param probes: The probes information.
-    :type probes: dict
     :return: Transformed probes information.
-    :rtype: OrderedDict
     """
     return OrderedDict(
         (
@@ -264,16 +244,31 @@ def transform_process_probes(probes):
     )
 
 
-def transform_bk_saas(bk_saas):
+def transform_bk_saas(bk_saas: List[Union[Dict[str, Any], str]]) -> List[Dict[str, Any]]:
     """
     Transforms the 'bk_saas' section in 'svc_discovery' of module.
 
     :param bk_saas: The 'bk_saas' section from spec_version 2 format.
-    :type bk_saas: list
     :return: Transformed 'bkSaas' list in specVersion 3 format.
-    :rtype: list
     """
     return [
         {snake_to_camel(k): v for k, v in item.items()} if isinstance(item, dict) else {"bkAppCode": item}
         for item in bk_saas
     ]
+
+
+def tranform_bkmonitor(processes: List[OrderedDict[str, Any]], bkmonitor_port: int) -> List[Dict[str, str]]:
+    """
+    Transforms module field 'bkmonitor', add to process services as monitoring metric.
+
+    :param processes: The transformed 'processes' in the specVersion 3 format.
+    :param bkmonitor_port: The 'port' in 'bkmonitor' of module from spec_version 2 format.
+    :return: Monitoring metrics list in specVersion 3 format.
+    """
+    for process in processes:
+        if process["name"] == "web":
+            process["services"].append(
+                {"name": "metrics", "protocol": "TCP", "port": bkmonitor_port, "targetPort": bkmonitor_port}
+            )
+            return [{"process": "web", "serviceName": "metrics", "path": "/metrics"}]
+    return []

8ef816a31b80f0cba2af0ecfb2a4332e6f7087c7
narasux <suzh9@mail2.sysu.edu.cn>
2025-01-03 15:20:02 +0800

    fix: update migrations

--- apiserver/paasng/paas_wl/infras/cluster/migrations/0012_clusterallocationpolicy_cluster_available_tenant_ids_and_more.py
+++ apiserver/paasng/paas_wl/infras/cluster/migrations/0012_clusterallocationpolicy_cluster_available_tenant_ids_and_more.py
@@ -12,7 +12,7 @@ import uuid
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cluster', '0011_cluster_exposed_url_type.py'),
+        ('cluster', '0011_cluster_exposed_url_type'),
     ]
 
     operations = [

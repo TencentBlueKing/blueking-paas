3551f30121b863b0c6666628a86baa592cd56ef5
piglei <piglei2007@gmail.com>
2025-01-17 15:46:13 +0800

    feat: add tenant_id field on market app

--- apiserver/paasng/paasng/core/tenant/fields.py
+++ apiserver/paasng/paasng/core/tenant/fields.py
@@ -10,23 +10,18 @@ def tenant_id_field_factory(db_index: bool = True, unique: bool = False) -> mode
 
     :param db_index: Whether to create an index for the field, defaults to True, turn
         it off when the model already has a compound index on the tenant_id field.
-    :param unique: Whether to create a unique index for the field, defaults to False. If unique is True,
-        param db_index will be ignored because unique implies the creation of an index.
+    :param unique: Whether to create a unique index for the field, defaults to False.
 
     NOTE: https://github.com/TencentBlueKing/blueking-paas/pull/1877#discussion_r1912873811 中有相关的设计讨论
     """
+    # If unique is True, db_index should be disabled to avoid creating a redundant index.
     if unique:
-        return models.CharField(
-            verbose_name="租户 ID",
-            max_length=32,
-            default=DEFAULT_TENANT_ID,
-            help_text="本条数据的所属租户",
-            unique=True,
-        )
+        db_index = False
     return models.CharField(
         verbose_name="租户 ID",
         max_length=32,
         default=DEFAULT_TENANT_ID,
         help_text="本条数据的所属租户",
         db_index=db_index,
+        unique=unique,
     )

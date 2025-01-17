76177b60dc6b02fbe23e50934f2b126bdf79e2ef
jamesgetx <gejun.coolfriend@gmail.com>
2025-01-17 10:16:29 +0800

    test: fix
    N

--- apiserver/paasng/tests/api/core/tenant/test_tenant.py
+++ apiserver/paasng/tests/api/core/tenant/test_tenant.py
@@ -14,38 +14,41 @@
 #
 # We undertake not to change the open source license (MIT license) applicable
 # to the current version of the project delivered to anyone in the future.
-
+from typing import List
 from unittest.mock import patch
 
+import cattrs
 import pytest
 from django.urls import reverse
 from rest_framework import status
+
+from paasng.infras.bk_user import entities
 
 
 class TestBCSResourceViewSet:
     @pytest.fixture(autouse=True)
     def _patch_bcs_api(self):
-        with patch(
-            "paasng.infras.bk_user.client.Client.api.list_tenants",
-            return_value={
-                "data": [
-                    {
-                        "id": "system",
-                        "name": "运营租户",
-                        "status": "enabled",
-                    },
-                    {
-                        "id": "blueking",
-                        "name": "蓝鲸",
-                        "status": "enabled",
-                    },
-                    {
-                        "id": "legacy",
-                        "name": "存量",
-                        "status": "disabled",
-                    },
-                ],
+        resp_data = [
+            {
+                "id": "system",
+                "name": "运营租户",
+                "status": "enabled",
             },
+            {
+                "id": "blueking",
+                "name": "蓝鲸",
+                "status": "enabled",
+            },
+            {
+                "id": "legacy",
+                "name": "存量",
+                "status": "disabled",
+            },
+        ]
+
+        with patch(
+            "paasng.core.tenant.views.BkUserClient.list_tenants",
+            return_value=cattrs.structure(resp_data, List[entities.Tenant]),
         ):
             yield
 

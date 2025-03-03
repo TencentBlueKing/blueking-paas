a22a611e8aaa746f714dbbedef100c555c916862
dijiachen <dijiachen@tencent.com>
2025-03-03 19:31:04 +0800

    feat: 前端特性添加沙箱

--- apiserver/paasng/paasng/misc/plat_config/views.py
+++ apiserver/paasng/paasng/misc/plat_config/views.py
@@ -46,8 +46,8 @@ class FrontendFeatureViewSet(ViewSet):
             "CNATIVE_MGRLEGACY": settings.FE_FEATURE_SETTINGS_CNATIVE_MGRLEGACY,
             # 应用令牌，用于 APP 调用用户态的云 API
             "APP_ACCESS_TOKEN": settings.FE_FEATURE_SETTINGS_APP_ACCESS_TOKEN,
-            # 是否开启沙箱开发
-            "DEV_SANDBOX": settings.FE_ENABLE_DEV_SANDBOX,
+            # 是否显示沙箱开发
+            "DEV_SANDBOX": settings.FE_FEATURE_SETTINGS_DEV_SANDBOX,
         }
         # 部分前端的特性复用了后端的配置
         features_reuses_backend_settings = {

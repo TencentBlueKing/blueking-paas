105a7b0024e0fda83355f4f49694c5cb2ea92c8f
dijiachen <dijiachen@tencent.com>
2025-03-11 16:57:05 +0800

    fix: 增强服务多租户 slz 修复

--- apiserver/paasng/paasng/plat_mgt/infras/services/serializers/binding_policies.py
+++ apiserver/paasng/paasng/plat_mgt/infras/services/serializers/binding_policies.py
@@ -29,8 +29,8 @@ class BaseAllocationPolicySLZ(serializers.Serializer):
     )
 
     def validate(self, attrs):
-        plans_exists = bool(getattr(attrs, "plans"))
-        env_plans_exists = bool(getattr(attrs, "env_plans"))
+        plans_exists = bool(attrs.get("plans", None))
+        env_plans_exists = bool(attrs.get("env_plans", None))
 
         if plans_exists == env_plans_exists:
             raise serializers.ValidationError("Must provide either plans or env_plans, but not both.")

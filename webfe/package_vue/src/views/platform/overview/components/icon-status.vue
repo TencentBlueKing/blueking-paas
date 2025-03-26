004b2e3fa985a2108a955738459c2066bedcd35e
leafage-collb <3303710797@qq.com>
2025-03-26 18:03:00 +0800

    fix: 多租户概览开发v1.0

---
+++ webfe/package_vue/src/views/platform/overview/components/icon-status.vue
@@ -0,0 +1,31 @@
+<template>
+  <span class="icon-status-wrapper">
+    <i
+      style="color: #18c0a1"
+      class="paasng-icon paasng-circle-correct-filled circle"
+      v-if="configured"
+    ></i>
+    <i
+      style="color: #c4c6cc"
+      class="paasng-icon paasng-time-filled"
+      v-else
+    ></i>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    configured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.icon-status-wrapper i {
+  font-size: 21px;
+}
+</style>

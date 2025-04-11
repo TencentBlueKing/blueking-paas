adfd57c7e4768c84fb1e69c63909e8efc578bbe9
narasux <suzh9@mail2.sysu.edu.cn>
2025-04-11 18:16:06 +0800

    refactor: initial_default_cluster command

--- apiserver/paasng/paas_wl/infras/cluster/management/commands/initial_default_cluster.py
+++ apiserver/paasng/paas_wl/infras/cluster/management/commands/initial_default_cluster.py
@@ -16,43 +16,72 @@
 # to the current version of the project delivered to anyone in the future.
 
 import logging
-import os
-from dataclasses import asdict, dataclass
 from typing import Dict, List
 
+from attr import define
+from blue_krill.data_types.enum import StrStructuredEnum
+from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from environ import Env
 
-from paas_wl.infras.cluster.constants import ClusterTokenType
-from paas_wl.infras.cluster.models import APIServer, Cluster
+from paas_wl.infras.cluster.constants import ClusterAllocationPolicyType, ClusterFeatureFlag, ClusterType
+from paas_wl.infras.cluster.entities import AllocationPolicy
+from paas_wl.infras.cluster.models import APIServer, Cluster, ClusterAllocationPolicy, ClusterElasticSearchConfig
+from paas_wl.infras.resources.base.base import invalidate_global_configuration_pool
+from paasng.core.tenant.user import get_init_tenant_id
 
 logger = logging.getLogger(__name__)
-
 
 env = Env()
 
 
-@dataclass
-class ClusterData:
-    name: str
-    description: str | None = None
-    ingress_config: Dict | None = None
-    annotations: Dict | None = None
-    ca_data: str | None = None
-    cert_data: str | None = None
-    key_data: str | None = None
-    token_type: ClusterTokenType | None = None
-    token_value: str | None = None
-    default_node_selector: Dict | None = None
-    default_tolerations: List | None = None
-    feature_flags: Dict | None = None
+class EnvVarKey(StrStructuredEnum):
+    """环境变量键名"""
+
+    APP_ROOT_DOMAIN = "PAAS_WL_CLUSTER_APP_ROOT_DOMAIN"
+    SUB_PATH_DOMAIN = "PAAS_WL_CLUSTER_SUB_PATH_DOMAIN"
+    HTTPS_ENABLED = "PAAS_WL_CLUSTER_ENABLED_HTTPS_BY_DEFAULT"
+    FRONTEND_INGRESS_IP = "PAAS_WL_CLUSTER_FRONTEND_INGRESS_IP"
+    HTTP_PORT = "PAAS_WL_CLUSTER_HTTP_PORT"
+    HTTPS_PORT = "PAAS_WL_CLUSTER_HTTPS_PORT"
+
+    BCS_CLUSTER_ID = "PAAS_WL_CLUSTER_BCS_CLUSTER_ID"
+    BCS_PROJECT_ID = "PAAS_WL_CLUSTER_BCS_PROJECT_ID"
+    BK_BIZ_ID = "PAAS_WL_CLUSTER_BK_BIZ_ID"
+
+    API_SERVER_URLS = "PAAS_WL_CLUSTER_API_SERVER_URLS"
+    CA = "PAAS_WL_CLUSTER_CA_DATA"
+    CERT = "PAAS_WL_CLUSTER_CERT_DATA"
+    KEY = "PAAS_WL_CLUSTER_KEY_DATA"
+    TOKEN = "PAAS_WL_CLUSTER_TOKEN_VALUE"
+
+    FEATURE_FLAGS = "PAAS_WL_CLUSTER_FEATURE_FLAGS"
+    NODE_SELECTOR = "PAAS_WL_CLUSTER_NODE_SELECTOR"
+    TOLERATIONS = "PAAS_WL_CLUSTER_TOLERATIONS"
 
 
-@dataclass
+@define
 class InitialClusterData:
-    cluster_id: str
-    cluster_data: ClusterData
-    api_server_urls: list
+    # 基础配置
+    uuid: str
+    name: str
+    tenant_id: str
+    description: str
+    ingress_config: Dict
+    annotations: Dict
+    # 访问凭证 - 证书
+    ca_data: str | None
+    cert_data: str | None
+    key_data: str | None
+    # 访问凭证 - Token
+    token_value: str | None
+    # 高级特性
+    default_node_selector: Dict
+    default_tolerations: List[Dict]
+    feature_flags: Dict
+    # ApiServer
+    api_server_urls: List[str]
 
 
 class Command(BaseCommand):
@@ -62,73 +91,165 @@ class Command(BaseCommand):
         parser.add_argument("--override", dest="override", action="store_true")
         parser.add_argument("--dry_run", dest="dry_run", action="store_true")
 
-    def render_data(self) -> InitialClusterData:
-        try:
-            data = {
-                "name": "default-main",
-                "description": "默认应用集群",
-                "ingress_config": {
-                    "default_ingress_domain_tmpl": "%s." + env.str("PAAS_WL_CLUSTER_SUB_PATH_DOMAIN", ""),
-                    "frontend_ingress_ip": env.str("PAAS_WL_CLUSTER_FRONTEND_INGRESS_IP", ""),
-                    "app_root_domains": [
-                        {
-                            "name": env.str("PAAS_WL_CLUSTER_APP_ROOT_DOMAIN", ""),
-                            "https_enabled": env.bool("PAAS_WL_CLUSTER_ENABLED_HTTPS_BY_DEFAULT", False),
-                        }
-                    ],
-                    "sub_path_domains": [
-                        {
-                            "name": env.str("PAAS_WL_CLUSTER_SUB_PATH_DOMAIN", ""),
-                            "https_enabled": env.bool("PAAS_WL_CLUSTER_ENABLED_HTTPS_BY_DEFAULT", False),
-                        }
-                    ],
-                    "port_map": {
-                        "http": env.int("PAAS_WL_CLUSTER_HTTP_PORT", 80),
-                        "https": env.int("PAAS_WL_CLUSTER_HTTPS_PORT", 443),
-                    },
-                },
-                "annotations": {
-                    "bcs_cluster_id": env.str("PAAS_WL_CLUSTER_BCS_CLUSTER_ID", ""),
-                    "bcs_project_id": env.str("PAAS_WL_CLUSTER_BCS_PROJECT_ID", ""),
-                    "bk_biz_id": env.str("PAAS_WL_CLUSTER_BK_BIZ_ID", ""),
-                },
-                "ca_data": env.str("PAAS_WL_CLUSTER_CA_DATA", ""),
-                "cert_data": env.str("PAAS_WL_CLUSTER_CERT_DATA", ""),
-                "key_data": env.str("PAAS_WL_CLUSTER_KEY_DATA", ""),
-                "token_type": 1,
-                "token_value": env.str("PAAS_WL_CLUSTER_TOKEN_VALUE", ""),
-                "default_node_selector": env.json("PAAS_WL_CLUSTER_NODE_SELECTOR", {}),
-                "default_tolerations": env.json("PAAS_WL_CLUSTER_TOLERATIONS", []),
-                "feature_flags": env.json("PAAS_WL_CLUSTER_FEATURE_FLAGS", {}),
-            }
-        except Exception as e:
-            raise ValueError("default cluster data is not valid") from e
-
-        cluster_id = "332d740b-03ed-40f2-aa6b-c90cc5f1e89c"
-        cluster_data = ClusterData(**data)
-
-        api_server_urls = os.environ.get("PAAS_WL_CLUSTER_API_SERVER_URLS")
-        api_server_list = api_server_urls.split(";") if api_server_urls else []
-
-        return InitialClusterData(cluster_id=cluster_id, cluster_data=cluster_data, api_server_urls=api_server_list)
-
     def handle(self, override, dry_run, *args, **options):
-        data = self.render_data()
-
-        cluster_qs = Cluster.objects.filter(pk=data.cluster_id)
-
-        if cluster_qs.exists() and not override:
-            logger.info("The cluster(pk:%s) already exists and overwriting is not allowed, skip", data.cluster_id)
-            return
+        data = self._build_cluster_data()
 
         if dry_run:
             logger.info("DRY-RUN: preparing to initialize the cluster, data: %s", data)
             return
 
-        cluster = Cluster.objects.register_cluster(pk=data.cluster_id, **asdict(data.cluster_data))
+        if Cluster.objects.filter(pk=data.uuid).exists() and not override:
+            logger.error("Cluster (uuid:%s, name:%s) exists and not allow to overwrite, skip", data.uuid, data.name)
+            return
 
-        for _url in data.api_server_urls:
-            APIServer.objects.get_or_create(cluster=cluster, host=_url, tenant_id=cluster.tenant_id)
+        # 更新 / 创建集群及相关资源配置
+        with transaction.atomic(using="workloads"):
+            self._upsert_cluster_and_related_resources(data)
 
-        APIServer.objects.exclude(host__in=data.api_server_urls).delete()
-        logger.info("The cluster was initialized successfully")
+        # 禁用内存中的集群资源池配置，使其重新加载
+        invalidate_global_configuration_pool()
+
+        logger.info("Successfully initialized the cluster %s in tenant %s", data.name, data.tenant_id)
+
+    def _build_cluster_data(self) -> InitialClusterData:
+        """根据环境变量 / 项目配置构建集群数据"""
+        # 网络相关配置
+        app_root_domain = env.str(EnvVarKey.APP_ROOT_DOMAIN, "")
+        sub_path_domain = env.str(EnvVarKey.SUB_PATH_DOMAIN, "")
+        # 至少需要配置 子域名 / 子路径 的域名
+        if not (app_root_domain or sub_path_domain):
+            raise ValueError(f"{EnvVarKey.APP_ROOT_DOMAIN} or {EnvVarKey.SUB_PATH_DOMAIN} must be set")
+
+        https_enabled = env.bool(EnvVarKey.HTTPS_ENABLED, False)
+        ingress_config = {
+            "default_ingress_domain_tmpl": "%s." + sub_path_domain,
+            "frontend_ingress_ip": env.str(EnvVarKey.FRONTEND_INGRESS_IP, ""),
+            "app_root_domains": (
+                [{"name": app_root_domain, "https_enabled": https_enabled}] if app_root_domain else []
+            ),
+            "sub_path_domains": (
+                [{"name": sub_path_domain, "https_enabled": https_enabled}] if sub_path_domain else []
+            ),
+            "port_map": {
+                "http": env.int(EnvVarKey.HTTP_PORT, 80),
+                "https": env.int(EnvVarKey.HTTPS_PORT, 443),
+            },
+        }
+
+        # Api 访问地址
+        api_server_urls = env.json(EnvVarKey.API_SERVER_URLS, [])
+        if not api_server_urls:
+            raise ValueError(f"{EnvVarKey.API_SERVER_URLS} (json list format) must be set")
+
+        # BCS 集群相关配置
+        bcs_cluster_id = env.str(EnvVarKey.BCS_CLUSTER_ID, "")
+        bcs_project_id = env.str(EnvVarKey.BCS_PROJECT_ID, "")
+        bk_biz_id = env.str(EnvVarKey.BK_BIZ_ID, "")
+
+        # 集群，项目，业务 ID 必须全都有值，或者全都无值
+        if bcs_cluster_id and bcs_project_id and bk_biz_id:
+            annotations = {
+                "bcs_cluster_id": bcs_cluster_id,
+                "bcs_project_id": bcs_project_id,
+                "bk_biz_id": bk_biz_id,
+            }
+        elif not bcs_cluster_id and not bcs_project_id and not bk_biz_id:
+            logger.info("No BCS cluster info found, set annotations as empty dict")
+            annotations = {}
+        else:
+            raise ValueError(
+                f"{EnvVarKey.BCS_CLUSTER_ID}, {EnvVarKey.BCS_PROJECT_ID}, "
+                + f"{EnvVarKey.BK_BIZ_ID} must be all set or all unset"
+            )
+
+        # 证书 / Token 相关配置
+        ca_data = env.str(EnvVarKey.CA, "")
+        cert_data = env.str(EnvVarKey.CERT, "")
+        key_data = env.str(EnvVarKey.KEY, "")
+        token_value = env.str(EnvVarKey.TOKEN, "")
+
+        if not ((ca_data and cert_data and key_data) or token_value):
+            raise ValueError(f"{EnvVarKey.CA}, {EnvVarKey.CERT}, {EnvVarKey.KEY} or {EnvVarKey.TOKEN} must be set")
+
+        # 集群功能特性
+        feature_flags = env.json(EnvVarKey.FEATURE_FLAGS, {})
+        if not feature_flags:
+            logger.info("No feature flags found, using default feature flags")
+            feature_flags = ClusterFeatureFlag.get_default_flags_by_cluster_type(ClusterType.NORMAL)
+
+        # 节点选择器 & 污点容忍度
+        node_selector = env.json(EnvVarKey.NODE_SELECTOR, {})
+        tolerations = env.json(EnvVarKey.TOLERATIONS, [])
+
+        return InitialClusterData(
+            # 固定 uuid & 集群名称
+            uuid="332d740b-03ed-40f2-aa6b-c90cc5f1e89c",
+            name="default-main",
+            tenant_id=get_init_tenant_id(),
+            description="default blueking application cluster",
+            ingress_config=ingress_config,
+            annotations=annotations,
+            ca_data=ca_data,
+            cert_data=cert_data,
+            key_data=key_data,
+            token_value=token_value,
+            default_node_selector=node_selector,
+            default_tolerations=tolerations,
+            feature_flags=feature_flags,
+            api_server_urls=api_server_urls,
+        )
+
+    def _upsert_cluster_and_related_resources(self, data: InitialClusterData):
+        # 1. 创建 / 更新集群
+        cluster, _ = Cluster.objects.update_or_create(
+            uuid=data.uuid,
+            name=data.name,
+            defaults={
+                "tenant_id": data.tenant_id,
+                "description": data.description,
+                "ingress_config": data.ingress_config,
+                "annotations": data.annotations,
+                "ca_data": data.ca_data,
+                "cert_data": data.cert_data,
+                "key_data": data.key_data,
+                "token_value": data.token_value,
+                "default_node_selector": data.default_node_selector,
+                "default_tolerations": data.default_tolerations,
+                "feature_flags": data.feature_flags,
+            },
+        )
+
+        # 2. 创建集群的 API Server，采用先删除后添加的方式
+        APIServer.objects.filter(cluster=cluster).delete()
+        APIServer.objects.bulk_create(
+            [APIServer(cluster=cluster, host=url, tenant_id=cluster.tenant_id) for url in data.api_server_urls]
+        )
+
+        # 3. 创建集群关联的日志配置
+        if not settings.ELASTICSEARCH_HOSTS:
+            raise ValueError("ELASTICSEARCH_HOSTS is not configured!")
+
+        conf = settings.ELASTICSEARCH_HOSTS[0]
+        host, port = conf["host"], conf["port"]
+        username, _, password = conf["http_auth"].partition(":")
+        scheme = "https" if conf.get("use_ssl") else "http"
+
+        ClusterElasticSearchConfig.objects.update_or_create(
+            cluster=cluster,
+            defaults={
+                "scheme": scheme,
+                "host": host,
+                "port": port,
+                "username": username,
+                "password": password,
+                "tenant_id": cluster.tenant_id,
+            },
+        )
+
+        # 4. 检查集群分配策略，若不存在，则创建默认的分配策略
+        if not ClusterAllocationPolicy.objects.filter(tenant_id=cluster.tenant_id).exists():
+            ClusterAllocationPolicy.objects.create(
+                tenant_id=cluster.tenant_id,
+                type=ClusterAllocationPolicyType.UNIFORM,
+                allocation_policy=AllocationPolicy(env_specific=False, clusters=[cluster.name]),
+            )

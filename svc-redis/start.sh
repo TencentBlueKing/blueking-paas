40962a782907b906686e367f1f58bafe84aa6714
dijiachen <dijiachen@tencent.com>
2025-04-21 20:06:34 +0800

    fix: Dockerfile 不加载 Data 目录

--- svc-redis/start.sh
+++ svc-redis/start.sh
@@ -23,10 +23,6 @@ set -e
 python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 
-# load fixtures
-# python manage.py loaddata data/fixtures/services.json
-# python manage.py loaddata data/fixtures/plans.json
-
 ## Run!
 command="gunicorn svc_redis.wsgi -w 4 -b [::]:5000 -k gevent --max-requests 2048 --access-logfile '-' --access-logformat '%(h)s %(l)s %(u)s %(t)s \"%(r)s\" %(s)s %(b)s \"%(f)s\" \"%(a)s\" in %(L)s seconds' --log-level INFO --log-file=-"
 exec bash -c "$command"

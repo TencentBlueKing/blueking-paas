e90592dde82784b076192b80f8ae6c1280c48235
jiayuan929 <252461528@qq.com>
2025-05-20 14:54:00 +0800

    fix: initial_data

--- apiserver/paasng/paasng/bk_plugins/pluginscenter/views.py
+++ apiserver/paasng/paasng/bk_plugins/pluginscenter/views.py
@@ -266,7 +266,7 @@ class PluginInstanceViewSet(PluginInstanceMixin, mixins.ListModelMixin, GenericV
         if is_ai_plugin:
             # 对于 AI 插件，直接从原始数据中获取 extra_fields
             # 因为蓝鲸应用的插件中并未定义 extraFields 的数据结构，前端针对 AI 插件的模板单独传的 extra_fields 字段
-            extra_fields = slz.data.get("extra_fields", {})
+            extra_fields = slz.initial_data.get("extra_fields", {})
 
         plugin = PluginInstance(
             pd=pd,

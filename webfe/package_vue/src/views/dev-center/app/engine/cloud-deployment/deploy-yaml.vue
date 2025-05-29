e38092485f39f552168fe833f8f366ab8b5f6735
wmh <3303710797@qq.com>
2025-05-29 11:33:58 +0800

    fix: 体验单据修复 (#2184)

--- webfe/package_vue/src/views/dev-center/app/engine/cloud-deployment/deploy-yaml.vue
+++ webfe/package_vue/src/views/dev-center/app/engine/cloud-deployment/deploy-yaml.vue
@@ -1,9 +1,7 @@
 <template>
   <paas-content-loader
-    :is-loading="screenIsLoading"
+    :is-loading="loading"
     placeholder="deploy-yaml-loading"
-    :offset-top="20"
-    :offset-left="20"
     class="deploy-action-box"
   >
     <div class="cloud-yaml-container">
@@ -12,8 +10,7 @@
         key="editor"
         v-model="detail"
         :readonly="true"
-        v-bkloading="{ isLoading, opacity: 1, color: '#1a1a1a' }"
-        :height="fullScreen ? clientHeight : height"
+        :height="height"
         @error="handleEditorErr"
       />
       <EditorStatus
@@ -24,7 +21,7 @@
     </div>
   </paas-content-loader>
 </template>
-<script>import appBaseMixin from '@/mixins/app-base-mixin.js';
+<script>
 import ResourceEditor from '@/components/deploy-resource-editor';
 import EditorStatus from '@/components/deploy-resource-editor/editor-status';
 export default {
@@ -32,7 +29,6 @@ export default {
     ResourceEditor,
     EditorStatus,
   },
-  mixins: [appBaseMixin],
   props: {
     cloudAppData: {
       type: Array,
@@ -42,70 +38,40 @@ export default {
       type: Number,
       default: 600,
     },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
   },
   data() {
     return {
-      fullScreen: false,
-      clientHeight: document.body.clientHeight,
-      isLoading: false,
       editorErr: {
         type: '',
         message: '',
       },
       detail: {},
-      screenIsLoading: true,
     };
-  },
-  computed: {
   },
   watch: {
     cloudAppData: {
       handler(val) {
         if (val.length) {
           this.$nextTick(() => {
-            setTimeout(() => {
-              this.detail = val[0];
-              this.$refs.editorRef?.setValue(val[0]);
-            }, 500);
+            this.detail = val[0];
+            this.$refs.editorRef?.setValue(val[0]);
           });
         }
       },
       immediate: true,
       deep: true,
     },
-    detail: {
-      handler(val) {
-        if (val && Object.keys(val).length) {
-          const webData = val.spec.processes.find(e => e.name === 'web');
-          if (!webData) {
-            this.handleEditorErr('至少需要一个web进程');
-          } else {
-            this.handleEditorErr();
-          }
-          setTimeout(() => {
-            this.screenIsLoading = false;
-          }, 500);
-        }
-      },
-      immediate: true,
-      deep: true,
-    },
-  },
-  mounted() {
   },
   methods: {
-    handleEditorErr(err) { // 捕获编辑器错误提示
+    // 捕获编辑器错误提示
+    handleEditorErr(err) {
       this.editorErr.type = 'content'; // 编辑内容错误
       this.editorErr.message = err;
-    },
-
-    formDataValidate(index) {
-      if (index) {
-        return false;
-      }
     },
   },
 };
 </script>
-<style scope>
-</style>

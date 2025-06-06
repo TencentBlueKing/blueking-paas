6de81307c6e84fc86fbe031ffcec9f35dc1e8022
jiayuan929 <252461528@qq.com>
2025-06-06 14:57:41 +0800

    fix: minor

--- apiserver/paasng/paasng/accessories/log/views/logs.py
+++ apiserver/paasng/paasng/accessories/log/views/logs.py
@@ -424,8 +424,7 @@ class ModuleLogAPIMixin(_MixinBase):
         prod_config = self._get_log_query_config_by_env(prod)
         if stag_config != prod_config:
             logging.exception(
-                "log query config mismatch between stag and prod, " "please check the config of module %s",
-                module,
+                "log query config mismatch between stag and prod, please check the config of module %s", module
             )
         return prod_config
 

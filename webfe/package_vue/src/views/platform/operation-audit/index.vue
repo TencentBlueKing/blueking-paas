1d3b06db5b2f5c2a22d3cc9fb48cf908af20ffaf
leafage-collb <3303710797@qq.com>
2025-06-13 15:54:26 +0800

    feat: 操作审计1.0 # Reviewed, transaction id: 46192

---
+++ webfe/package_vue/src/views/platform/operation-audit/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="right-main operation-audit-container">
+    <Record :type="tabActive" />
+  </div>
+</template>
+
+<script>
+import Record from './record';
+
+export default {
+  name: 'PlatformOperationAudit',
+  components: {
+    Record,
+  },
+  computed: {
+    tabActive() {
+      return this.$route.query.active || 'platform';
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.operation-audit-container {
+  padding: 24px;
+}
+</style>

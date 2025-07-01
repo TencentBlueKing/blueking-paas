b3a6410de52428260d0f77aab2018e057f5c6512
narasux <suzh9@mail2.sysu.edu.cn>
2025-07-01 12:01:18 +0800

    fix: resolve #2260 conversations

--- webfe/package_vue/src/views/platform/operations/app-list/index.vue
+++ webfe/package_vue/src/views/platform/operations/app-list/index.vue
@@ -319,7 +319,9 @@ export default {
       };
       // 已下架的应用永远排在最后
       if (queryParams.order_by) {
-        queryParams.order_by = `-is_active,${queryParams.order_by}`
+        queryParams.order_by = `-is_active,${queryParams.order_by}`;
+      } else {
+        queryParams.order_by = '-is_active';
       }
       if (this.searchValue) {
         queryParams.search = this.searchValue;

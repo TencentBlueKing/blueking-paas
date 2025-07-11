ac705f1e3f37dd6481b9e5c742453b2628336d38
piglei <piglei2007@gmail.com>
2025-07-11 20:54:08 +0800

    refactor: add path checking when detetor determine relative path (#2320)

---
+++ apiserver/paasng/paasng/utils/file.py
@@ -0,0 +1,31 @@
+# TencentBlueKing is pleased to support the open source community by making
+# 蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
+# Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+# Licensed under the MIT License (the "License"); you may not use this file except
+# in compliance with the License. You may obtain a copy of the License at
+#
+#     http://opensource.org/licenses/MIT
+#
+# Unless required by applicable law or agreed to in writing, software distributed under
+# the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+# either express or implied. See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# We undertake not to change the open source license (MIT license) applicable
+# to the current version of the project delivered to anyone in the future.
+"""file and path related utilities."""
+
+import os
+
+from paasng.utils.text import generate_random_string
+
+
+def path_may_escape(input_path: str) -> bool:
+    """Check if the input path may escape from a given root directory, an invalid value is like
+    "../../../../app_desc.yaml" which can go outside the root directory.
+    """
+    # Absolute paths are always considered as escaping
+    if os.path.isabs(input_path):
+        return True
+    sim_root = f"/var/folders/{generate_random_string(12)}/T"
+    return os.path.commonpath([os.path.abspath(os.path.join(sim_root, input_path)), sim_root]) != sim_root

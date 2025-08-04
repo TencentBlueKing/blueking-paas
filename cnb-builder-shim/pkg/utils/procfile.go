00b931a24731b2f10b1ae48a978a43d15e59a273
narasux <suzh9@mail2.sysu.edu.cn>
2025-08-04 14:50:09 +0800

    docs: update GenBashCommandWithTokens comments

--- cnb-builder-shim/pkg/utils/procfile.go
+++ cnb-builder-shim/pkg/utils/procfile.go
@@ -34,28 +34,29 @@ import (
 //
 // After token evaluation, the process will run as a Procfile like: 'python -m http.server ${PORT:-9999}'
 //
-// ref: https://github.com/buildpacks/lifecycle/blob/435d226f1ed54b0bec806716ba79e14a2a093736/launch/bash.go#L55
+// ref:
+//   - https://github.com/buildpacks/lifecycle/blob/435d226f1ed54b0bec806716ba79e14a2a093736/launch/bash.go#L55
+//   - https://github.com/TencentBlueKing/blueking-paas/blob/95b99c5a1a41d31efa3b2270968bfc846102cfcb/apiserver/paasng/paasng/utils/procfile.py#L22
 func GenBashCommandWithTokens(command []string, args []string) string {
-	// 合并所有 token
+	// 将 command 和 args 都认为 bash 命令的参数
 	tokens := append(command, args...)
 	tokenSize := len(tokens)
 
-	// 构建内部脚本核心逻辑：
-	// 使用 eval echo 展开每个位置参数（$0, $1, $2...）
+	// 使用 eval echo 展开每个位置参数（$0, $1, $2...），环境变量亦会生效
 	script := `"$(eval echo \"$0\")"`
 	for i := 1; i < tokenSize; i++ {
 		script += fmt.Sprintf(` "$(eval echo \"${%d}\")"`, i)
 	}
 
-	// 引用所有 token
-	var quotedTokens strings.Builder
+	// command + args 作为参数，前后添加单引号
+	var sb strings.Builder
 	for _, token := range tokens {
-		quotedTokens.WriteString(" ")
-		quotedTokens.WriteString(shQuote(token))
+		sb.WriteString(" ")
+		sb.WriteString(shQuote(token))
 	}
 
-	// 组合最终命令：bash -c '核心脚本' + 引用后的参数
-	return "bash -c " + shQuote(script) + quotedTokens.String()
+	// 拼接结果：bash -c '核心脚本' + 引用后的参数
+	return "bash -c " + shQuote(script) + sb.String()
 }
 
 // shQuote 将字符串进行单引号转义，用于安全嵌入 Bash 命令

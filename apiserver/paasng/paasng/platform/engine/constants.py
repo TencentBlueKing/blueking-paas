97607d4bff6f4b557b81bd8a7f05c34917fe1112
jamesgetx <gejun.coolfriend@gmail.com>
2025-08-12 15:58:31 +0800

    refactor: use replicas_override_policy instead of lock_replicas to support replicas override
    N

--- apiserver/paasng/paasng/platform/engine/constants.py
+++ apiserver/paasng/paasng/platform/engine/constants.py
@@ -142,3 +142,10 @@ class RuntimeType(StrStructuredEnum):
     BUILDPACK = EnumField("buildpack", label=_("使用 Buildpacks 构建"))
     DOCKERFILE = EnumField("dockerfile", label=_("使用 Dockerfile 构建"))
     CUSTOM_IMAGE = EnumField("custom_image", label="Custom Image(云原生和旧镜像应用)")
+
+
+class ReplicasOverridePolicy(StrStructuredEnum):
+    """部署时副本数的优先级策略"""
+
+    WEB_FORM = EnumField("web_form", label=_("页面配置优先"))
+    APP_DESC = EnumField("app_desc", label=_("描述文件优先"))

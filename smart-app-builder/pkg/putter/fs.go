d61e9447fde79f717fd68d3c7dbc20c56e338ad6
xogas <57179186+xogas@users.noreply.github.com>
2025-08-22 16:16:00 +0800

    feat: smart-app-builder 支持上传 smart 包到 bkrepo (#2425)

---
+++ smart-app-builder/pkg/putter/fs.go
@@ -0,0 +1,56 @@
+/*
+ * TencentBlueKing is pleased to support the open source community by making
+ * 蓝鲸智云 - PaaS 平台 (BlueKing - PaaS System) available.
+ * Copyright (C) 2017 THL A29 Limited, a Tencent company. All rights reserved.
+ * Licensed under the MIT License (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://opensource.org/licenses/MIT
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * We undertake not to change the open source license (MIT license) applicable
+ * to the current version of the project delivered to anyone in the future.
+ */
+
+package putter
+
+import (
+	"net/url"
+	"os"
+	"path/filepath"
+
+	"github.com/TencentBlueking/bkpaas/smart-app-builder/pkg/utils"
+	"github.com/go-logr/logr"
+)
+
+// FsPutter ...
+type FsPutter struct {
+	Logger logr.Logger
+}
+
+// NewFsPutter ...
+func NewFsPutter(log logr.Logger) *FsPutter {
+	return &FsPutter{Logger: log}
+}
+
+// Put src from local filesystem to destUrl
+func (f *FsPutter) Put(src string, destUrl *url.URL) error {
+	filePath := destUrl.Path
+	fileName := filepath.Base(filePath)
+	if filepath.Ext(fileName) == ".tgz" {
+		if err := os.MkdirAll(filepath.Dir(filePath), 0o744); err != nil {
+			return err
+		}
+		return utils.CopyFile(src, filePath)
+	}
+
+	// Assume dest is a directory, otherwise an error occurs.
+	if err := os.MkdirAll(filePath, 0o744); err != nil {
+		return err
+	}
+	return utils.CopyFile(src, filepath.Join(filePath, filepath.Base(src)))
+}

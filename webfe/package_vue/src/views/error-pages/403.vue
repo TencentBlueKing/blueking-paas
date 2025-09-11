41ad446fcca69e635989fe0b442fabcddc3cd6c8
wmh <3303710797@qq.com>
2025-09-11 16:34:09 +0800

    fix：403页面样式修复 (#2500)

--- webfe/package_vue/src/views/error-pages/403.vue
+++ webfe/package_vue/src/views/error-pages/403.vue
@@ -1,13 +1,13 @@
 <template lang="html">
   <div class="right-main">
-    <div class="overview-tit">
-      <h2>403</h2>
-    </div>
     <div
       v-if="errorDetail.code === 1302403"
       class="no-app-permissions"
     >
-      <bk-exception class="exception-wrap-item" type="403">
+      <bk-exception
+        class="exception-wrap-item"
+        type="403"
+      >
         <span>{{ $t('无该应用访问权限') }}</span>
         <div class="text-subtitle">{{ errorDetail.detail }}</div>
       </bk-exception>
@@ -19,10 +19,10 @@
     >
       <div
         class="nofound"
-        style="width: 1180px; margin: 0 auto;"
+        style="width: 1180px; margin: 0 auto"
       >
-        <img src="/static/images/permissions.png">
-        <p> {{ $t('您没有访问当前应用该功能的权限') }} </p>
+        <img src="/static/images/permissions.png" />
+        <p>{{ $t('您没有访问当前应用该功能的权限') }}</p>
         <bk-button
           v-if="applyUrl"
           :theme="'primary'"
@@ -60,9 +60,12 @@ export default {
     },
   },
   async created() {
-    let appInfo = {}
+    let appInfo = {};
     if (this.isPlugin) {
-      appInfo = await this.$store.dispatch('plugin/getPluginInfo', { pluginId: this.id, pluginTypeId: this.pluginTypeId });
+      appInfo = await this.$store.dispatch('plugin/getPluginInfo', {
+        pluginId: this.id,
+        pluginTypeId: this.pluginTypeId,
+      });
     } else {
       appInfo = await this.$store.dispatch('getAppInfo', { appCode: this.id });
     }
@@ -73,7 +76,7 @@ export default {
       window.open(this.applyUrl, '_blank');
     },
     // 应用是否有权限
-    isPermission (data) {
+    isPermission(data) {
       // 403 无权限
       if (data.status === 403 || data.response?.status === 403) {
         return;
@@ -83,44 +86,44 @@ export default {
           name: 'pluginSummary',
           params: {
             id: this.id,
-            pluginTypeId: this.pluginTypeId
-          }
+            pluginTypeId: this.pluginTypeId,
+          },
         });
       } else {
         // 跳转概览/默认模块
         this.$router.push({
           name: 'appSummary',
           params: {
-            id: this.id
-          }
+            id: this.id,
+          },
         });
       }
-    }
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-    .nofound {
-        width: 939px;
-        padding-top: 150px;
-        text-align: center;
-    }
+.nofound {
+  width: 939px;
+  padding-top: 150px;
+  text-align: center;
+}
 
-    .nofound p {
-        font-size: 20px;
-        color: #979797;
-        line-height: 80px;
-    }
+.nofound p {
+  font-size: 20px;
+  color: #979797;
+  line-height: 80px;
+}
 
-    .no-app-permissions {
-        margin-top: 150px;
+.no-app-permissions {
+  margin-top: 150px;
 
-        .text-subtitle {
-            color: #979ba5;
-            font-size: 14px;
-            text-align: center;
-            margin-top: 14px;
-        }
-    }
+  .text-subtitle {
+    color: #979ba5;
+    font-size: 14px;
+    text-align: center;
+    margin-top: 14px;
+  }
+}
 </style>

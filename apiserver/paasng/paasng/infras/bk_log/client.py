acd26061a36550a1a445d61ca118d09bcf5ee2fd
xogas <57179186+xogas@users.noreply.github.com>
2025-11-14 18:09:51 +0800

    fix: review 意见修改

--- apiserver/paasng/paasng/infras/bk_log/client.py
+++ apiserver/paasng/paasng/infras/bk_log/client.py
@@ -146,7 +146,7 @@ class BkLogManagementClient:
                     "storage_cluster_id": config.storage_config.storage_cluster_id,
                     "retention": config.storage_config.retention,
                     "es_shards": config.storage_config.es_shards,
-                    # TODO: 后端接口参数拼写错误，先沿用
+                    # 后端接口参数拼写错误，先沿用
                     "storage_replies": config.storage_config.storage_replicas,
                     "allocation_min_days": config.storage_config.allocation_min_days,
                 }
@@ -189,7 +189,7 @@ class BkLogManagementClient:
                     "storage_cluster_id": config.storage_config.storage_cluster_id,
                     "retention": config.storage_config.retention,
                     "es_shards": config.storage_config.es_shards,
-                    # TODO: 后端接口参数拼写错误，先沿用
+                    # 后端接口参数拼写错误，先沿用
                     "storage_replies": config.storage_config.storage_replicas,
                     "allocation_min_days": config.storage_config.allocation_min_days,
                 }

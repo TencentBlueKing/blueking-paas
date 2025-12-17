ae9ef793dd046eeb6b37d673784aec30f21c1b71
xogas <57179186+xogas@users.noreply.github.com>
2025-12-17 16:18:32 +0800

    test: 修复测试

--- apiserver/paasng/tests/paas_wl/bk_app/processes/test_processes.py
+++ apiserver/paasng/tests/paas_wl/bk_app/processes/test_processes.py
@@ -158,14 +158,12 @@ def test_list_cnative_module_processes_specs(
         ],
     ):
         specs = list_cnative_module_processes_specs(bk_cnative_app, "stag")
-        # When override_config is set, plan_name becomes "customized"
-        expected_plan_name = "customized" if override_config else "4C4G"
         assert specs["default"] == [
             {
                 "name": "web",
                 "max_replicas": 10,
                 "target_replicas": 1,
-                "plan_name": expected_plan_name,
+                "plan_name": "4C4G",
                 "autoscaling": False,
                 "scaling_config": None,
                 "target_status": "start",
@@ -276,7 +274,7 @@ class TestProcessManager:
                 "name": "foo",
                 "max_replicas": 10,
                 "target_replicas": 1,
-                "plan_name": "customized",
+                "plan_name": "default",
                 "autoscaling": False,
                 "scaling_config": None,
                 "target_status": "start",

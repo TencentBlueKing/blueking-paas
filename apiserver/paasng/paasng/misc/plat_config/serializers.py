2f2f925d8e67d23c08fc7578a7adc55c6fe35dab
xiaoj655 <xiaoj655@gmail.com>
2026-01-08 15:40:46 +0800

    fix

--- apiserver/paasng/paasng/misc/plat_config/serializers.py
+++ apiserver/paasng/paasng/misc/plat_config/serializers.py
@@ -4,7 +4,4 @@ from rest_framework import serializers
 class EncryptConfigSLZ(serializers.Serializer):
     enabled = serializers.BooleanField(help_text="是否启用前端加密")
     public_key = serializers.CharField(allow_null=True)
-    encrypt_field_prefix = serializers.CharField(
-        allow_null=True, help_text="加密的字段前缀, 用于区分字段值是否被加密过"
-    )
     encrypt_cipher_type = serializers.ChoiceField(allow_null=True, choices=["SM2"], help_text="加密算法类型")

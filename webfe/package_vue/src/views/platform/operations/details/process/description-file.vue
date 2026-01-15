f5c66a5333c57711fc2b517e64be3aa1b74e8432
wmh <3303710797@qq.com>
2026-01-15 14:25:28 +0800

    feat：平台管理-Smart应用资源配额：支持恢复以应用描述文件为准，兼容调整后的接口数据结构 (#2639)

---
+++ webfe/package_vue/src/views/platform/operations/details/process/description-file.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <SectionContainer
+      :title="$t('预发布环境')"
+      class="mb-24"
+    >
+      <DetailsRow
+        :label-width="labelWidth"
+        :label="$t('资源配额方案')"
+      >
+        <div slot="value">
+          <template v-if="!isEdit">
+            {{ stagPlanName || '--' }}
+          </template>
+          <bk-select
+            v-else
+            v-model="stagPlanName"
+            style="width: 300px"
+            ext-cls="plan-select-cls"
+            :disabled="true"
+          >
+            <bk-option
+              :id="stagPlanName"
+              :name="stagPlanName"
+            ></bk-option>
+          </bk-select>
+        </div>
+      </DetailsRow>
+    </SectionContainer>
+    <SectionContainer :title="$t('生产环境')">
+      <DetailsRow
+        :label-width="labelWidth"
+        :label="$t('资源配额方案')"
+      >
+        <div slot="value">
+          <template v-if="!isEdit">
+            {{ prodPlanName || '--' }}
+          </template>
+          <bk-select
+            v-else
+            v-model="prodPlanName"
+            style="width: 300px"
+            ext-cls="plan-select-cls"
+            :disabled="true"
+          >
+            <bk-option
+              :id="prodPlanName"
+              :name="prodPlanName"
+            ></bk-option>
+          </bk-select>
+        </div>
+      </DetailsRow>
+    </SectionContainer>
+  </div>
+</template>
+
+<script>
+import SectionContainer from '@/components/section-container';
+import DetailsRow from '@/components/details-row';
+
+export default {
+  name: 'DescriptionFile',
+  components: {
+    SectionContainer,
+    DetailsRow,
+  },
+  props: {
+    processData: {
+      type: Object,
+      default: () => {},
+    },
+    isEdit: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      labelWidth: 120,
+    };
+  },
+  computed: {
+    stagPlanName() {
+      return this.processData?.env_overlays?.stag?.plan_name || '';
+    },
+    prodPlanName() {
+      return this.processData?.env_overlays?.prod?.plan_name || '';
+    },
+  },
+};
+</script>

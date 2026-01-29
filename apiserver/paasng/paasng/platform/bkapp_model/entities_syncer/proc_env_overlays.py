eb9baa2442bc90f93c2cb8db42d33cac2dafb057
xogas <57179186+xogas@users.noreply.github.com>
2026-01-29 11:25:09 +0800

    fix: 新增字段后清理过程异常

--- apiserver/paasng/paasng/platform/bkapp_model/entities_syncer/proc_env_overlays.py
+++ apiserver/paasng/paasng/platform/bkapp_model/entities_syncer/proc_env_overlays.py
@@ -84,6 +84,8 @@ def clean_empty_overlays(module):
         "plan_name": None,
         "override_plan_name": None,
         "override_resources": None,
+        # override_proc_res 模型字段已经废弃, 但为了防止新增字段后清理过程异常, 保留该字段的检查
+        "override_proc_res": None,
     }
 
     # If new fields were added to the module, the clean up process should abort to avoid data loss.

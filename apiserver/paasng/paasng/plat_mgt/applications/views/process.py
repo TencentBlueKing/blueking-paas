fb6e9a5e7f14f1d7e2b0954454b6c685b4ae627c
xogas <57179186+xogas@users.noreply.github.com>
2026-03-16 15:52:14 +0800

    feat: ai-agent support edit source plan in plat-mgt

--- apiserver/paasng/paasng/plat_mgt/applications/views/process.py
+++ apiserver/paasng/paasng/plat_mgt/applications/views/process.py
@@ -100,9 +100,9 @@ class ApplicationProcessViewSet(viewsets.GenericViewSet):
 
         # 校验 module 的 source_origin 是否可以修改
         module = get_object_or_404(application.modules, name=module_name)
-        if module.source_origin != SourceOrigin.S_MART.value:
+        if module.source_origin != SourceOrigin.S_MART.value and application.is_ai_agent_app is False:
             return Response(
-                {"detail": _("当前仅支持 SMart 应用修改进程资源配额")},
+                {"detail": _("当前仅支持 SMart 和 AI Agent 应用修改进程资源配额")},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 

841cd4d56ecadba30f4e2b5cc6a6a8cc1b90592c
xogas <57179186+xogas@users.noreply.github.com>
2026-07-21 20:10:21 +0800

    revert: revert python and opentelemetry version (#2968)

--- apiserver/paasng/paasng/misc/tracing/instrumentor.py
+++ apiserver/paasng/paasng/misc/tracing/instrumentor.py
@@ -30,15 +30,15 @@ from requests.models import Response
 
 
 def requests_callback(span: Span, request, response: Optional[Response]):
-    """处理蓝鲸标准协议响应"""
+    """
+    处理蓝鲸标准协议响应
+    """
     # requests 请求异常, 例如访问超时等
     if response is None:
         return
 
     # 并非所有返回内容都是 json 格式的, 因此需要根据返回头进行判断, 避免处理二进制格式的内容
-    # 使用 split 分离 media type 和参数 (如 charset), 兼容 "application/json; charset=utf-8" 等变体
-    content_type = response.headers.get("Content-Type", "")
-    if content_type.split(";")[0].strip().lower() != "application/json":
+    if response.headers.get("Content-Type") != "application/json":
         return
 
     try:
@@ -80,7 +80,9 @@ def requests_callback(span: Span, request, response: Optional[Response]):
 
 
 def django_response_hook(span: Span, request, response):
-    """处理 Django 响应"""
+    """
+    处理 Django 响应
+    """
     if response.status_code == 200:
         span.set_status(Status(StatusCode.OK))
     else:
@@ -96,7 +98,7 @@ class BKAppInstrumentor(BaseInstrumentor):
 
     def _instrument(self, **kwargs):
         LoggingInstrumentor().instrument()
-        RequestsInstrumentor().instrument(response_hook=requests_callback)
+        RequestsInstrumentor().instrument(span_callback=requests_callback)
         DjangoInstrumentor().instrument(response_hook=django_response_hook)
         RedisInstrumentor().instrument()
         CeleryInstrumentor().instrument()

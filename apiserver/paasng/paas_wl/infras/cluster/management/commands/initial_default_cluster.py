18dc41edc7ba43723af5a2e547cc07fe007a67ff
Kailong Jiang <xiaoj655@gmail.com>
2026-08-06 03:46:55 +0000

    fix

--- apiserver/paasng/paas_wl/infras/cluster/management/commands/initial_default_cluster.py
+++ apiserver/paasng/paas_wl/infras/cluster/management/commands/initial_default_cluster.py
@@ -134,4 +134,5 @@ class Command(BaseCommand):
 
         for _url in data.api_server_urls:
             APIServer.objects.get_or_create(cluster=cluster, host=_url)
+        APIServer.objects.filter(cluster=cluster).exclude(host__in=data.api_server_urls).delete()
         logger.info("The cluster was initialized successfully")

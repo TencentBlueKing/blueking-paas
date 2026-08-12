a2d334aa042ce24fa0faf9bac770722eba038233
xogas <57179186+xogas@users.noreply.github.com>
2026-08-12 17:10:40 +0800

    fix: some comment error

--- apiserver/paasng/paas_wl/utils/models.py
+++ apiserver/paasng/paas_wl/utils/models.py
@@ -52,7 +52,7 @@ class Char32UUIDField(models.UUIDField):
 
     def get_db_prep_value(self, value, connection, prepared=False):
         value = super().get_db_prep_value(value, connection, prepared)
-        if value is not None:
+        if isinstance(value, uuid.UUID):
             value = value.hex
         return value
 
